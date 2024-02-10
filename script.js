5c39c928fd20b4c1e7f286f7b903de53113f339b
Ingo Wolf <wolfingo28@gmail.com>
2024-02-10 15:56:41 +1100

    add scrolltotop

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const swiper = new Swiper(".swiper", {
     delay: 1000,
   },
 });
+
+function scrollDown() {
+  const s = window.scrollY,
+    e = Math.min(
+      s + window.innerHeight,
+      document.body.scrollHeight - window.innerHeight
+    ),
+    t = performance.now();
+  requestAnimationFrame(function o(i) {
+    const r = (i - t) / 1e3,
+      c = r >= 1 ? 1 : 1 - Math.pow(2, -10 * r);
+    window.scrollTo(0, s + (e - s) * c);
+    if (r < 1) requestAnimationFrame(o);
+  });
+}
+
