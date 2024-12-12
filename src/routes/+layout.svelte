9aded26826800053b392c0650360139955803f07
Ingo <me@ingowolf.au>
2024-12-12 18:24:15 +1100

    shift trigger right a bit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,6 @@
 	<AppSidebar />
 	<Sidebar.Inset>
 		{@render children()}
-		<Sidebar.Trigger class="fixed bottom-2" />
+		<Sidebar.Trigger class="fixed bottom-2 translate-x-2" />
 	</Sidebar.Inset>
 </Sidebar.Provider>
