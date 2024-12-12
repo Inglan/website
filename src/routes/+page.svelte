d6ac5077f14c33665ac2f4937d7e2d26b9d5ea77
Ingo <me@ingowolf.au>
2024-12-12 20:24:38 +1100

    modify sidebar trigger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,19 +20,20 @@
 		var lastScrollY = 0;
 		if (window.scrollY < sidebarScrollYThreshold) {
 			hideSidebar();
+			document.getElementById('sidebar-trigger')?.classList.remove('bg-secondary');
 		}
 		const handleScroll = () => {
 			if (!sidebar.isMobile) {
 				const currentScrollY = window.scrollY;
 				if (sidebarScrollYThreshold < currentScrollY && sidebarScrollYThreshold > lastScrollY) {
 					showSidebar();
-					document.getElementById('sidebar-trigger')?.classList.remove('opacity-25');
+					document.getElementById('sidebar-trigger')?.classList.add('bg-secondary');
 				} else if (
 					sidebarScrollYThreshold > currentScrollY &&
 					sidebarScrollYThreshold < lastScrollY
 				) {
 					hideSidebar();
-					document.getElementById('sidebar-trigger')?.classList.add('opacity-25');
+					document.getElementById('sidebar-trigger')?.classList.remove('bg-secondary');
 				}
 			}
 			lastScrollY = window.scrollY;
