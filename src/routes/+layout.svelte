19cc1195d6d9e4c5da6b7645318d751d457ba835
Ingo <me@ingowolf.au>
2024-12-12 17:31:38 +1100

    add sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
-{@render children()}
+<Sidebar.Provider style="--sidebar-width: 19rem;">
+	<AppSidebar />
+	<Sidebar.Inset>
+		{@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
