693a5b6bbfab2fa63b6e73296c015203ec9c4e77
Ingo <me@ingowolf.au>
2025-01-06 12:50:50 +1100

    remove text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -149,12 +149,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <style>
 	.colored {
 		background: linear-gradient(45deg, #fab387, #f38ba8);
