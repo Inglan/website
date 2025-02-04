ebfc4b6458cc10557d5c4a223b4229c3e5a9a451
Ingo <me@ingowolf.au>
2025-02-04 16:37:22 +1100

    update projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,16 +5,19 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-{#each data.projects as project}
-	<Card.Root>
-		<Card.Header>
-			<Card.Title>{project.title}</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			{project.excerpt}
-		</Card.Content>
-		<Card.Footer>
-			<Button href={project.slug}>Read More</Button>
-		</Card.Footer>
-	</Card.Root>
-{/each}
+<div class="m-auto grid max-w-screen-lg grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
+	<h1 class="flex items-center justify-center">Projects</h1>
+	{#each data.projects as project}
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>{project.title}</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				{project.excerpt}
+			</Card.Content>
+			<Card.Footer>
+				<Button href={project.slug}>Read More</Button>
+			</Card.Footer>
+		</Card.Root>
+	{/each}
+</div>
