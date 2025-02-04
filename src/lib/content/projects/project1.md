708024ec3f2472fced9b7668095b4893a32f0691
Ingo <me@ingowolf.au>
2025-02-04 15:59:24 +1100

    make projects page better

--- src/lib/content/projects/project1.md
+++ src/lib/content/projects/project1.md
@@ -1,5 +1,6 @@
 ---
 title: Project 1
+excerpt: A test page
 ---
 
 # H1
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	export let data;
+
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
 {#each data.projects as project}
-	<a href={project.slug}>
-		{project.title}
-	</a>
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>{project.title}</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			{project.excerpt}
+		</Card.Content>
+		<Card.Footer>
+			<Button href={project.slug}>Read More</Button>
+		</Card.Footer>
+	</Card.Root>
 {/each}
