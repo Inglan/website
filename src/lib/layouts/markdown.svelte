1410aea2e7e3fc60a594c7ba45faa65e67e0940a
Ingo <me@ingowolf.au>
2025-02-14 21:59:45 +1100

    Add svelte seo to markdown template

--- src/lib/layouts/markdown.svelte
+++ src/lib/layouts/markdown.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	export let title;
+
+	import SvelteSeo from 'svelte-seo';
 </script>
+
+<SvelteSeo {title} />
 
 <article class="m-auto max-w-screen-lg p-4">
 	<h1>{title}</h1>
