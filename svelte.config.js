7932a1a9cda5f50723adc04af38ea97e2095953a
Inglan <Inglan@users.noreply.github.com>
2025-02-15 04:30:31 +0000

    Prettified Code!

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from "mdsvex";
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex({
-		extensions: ['.md'],
-		layout: './src/lib/layouts/markdown.svelte',
-	})],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: './src/lib/layouts/markdown.svelte'
+		})
+	],
 
 	extensions: ['.svelte', '.md'],
 
