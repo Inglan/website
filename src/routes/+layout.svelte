aaf16590854b73ecda0bc108bc1d04e10b8ceb89
Ingo <me@ingowolf.au>
2025-02-16 17:03:42 +1100

    chagne styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,9 +53,7 @@
 					</main>
 				{/key}
 				<!-- Sidebar Trigger Button -->
-				<div
-					class="fixed bottom-2 flex translate-x-2 flex-row gap-2 [&>button]:p-6 [&>button]:backdrop-blur-2xl"
-				>
+				<div class="fixed bottom-2 flex translate-x-2 flex-row gap-2 *:p-6 *:backdrop-blur-2xl">
 					<Sidebar.Trigger id="sidebar-trigger" />
 					<Button type="button" variant="ghost" size="icon" href="/">
 						<Home />
