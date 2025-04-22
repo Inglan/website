798fb6007e16fb136fadd3663defc390461f281b
Ingo Wolf <me@ingowolf.au>
2025-04-22 13:27:36 +1000

    style menubutton more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,7 @@
 	import Menu from '$lib/components/Menu.svelte';
 </script>
 
-<div class="fixed top-5 right-5 flex h-15 w-15 rounded-full bg-white" id="menu"></div>
+<div
+	class="bg-surface1/50 fixed top-5 right-5 flex h-15 w-15 rounded-full backdrop-blur-lg"
+	id="menu"
+></div>
