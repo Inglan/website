4f70451cd930bbd24311dd992b2f78b009b7ccc6
Ingo Wolf <me@ingowolf.au>
2025-04-22 14:04:13 +1000

    add onclick

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 
 <div
 	class="bg-surface1/50 fixed top-5 right-5 flex h-15 w-15 items-center justify-center rounded-full backdrop-blur-lg duration-200 hover:scale-[1.1] active:scale-[0.95]"
+	onclick={() => (menuOpen = !menuOpen)}
 	id="menutrigger"
 >
 	<div
