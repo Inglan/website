8d7cba7fd216cc6e7b1b07d432cfb55168fda6a9
Ingo Wolf <me@ingowolf.au>
2025-04-22 14:35:08 +1000

    fix menu transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <div
 	class={clsx(
-		'fixed top-5 right-5 flex h-20 w-20 rounded-full backdrop-blur-lg duration-1000',
+		'fixed top-5 right-5 flex h-20 w-20 rounded-full backdrop-blur-lg',
 		menuState.open && 'bg-surface1/50 open'
 	)}
 	id="menubackground"
@@ -37,10 +37,24 @@
 </button>
 
 <style>
+	#menubackground {
+		transition:
+			width 1s,
+			height 1s,
+			top 1s,
+			right 1s,
+			background-color 1s 1s;
+	}
 	#menubackground.open {
 		width: calc(max(100vw, 100vh) * 4);
 		height: calc(max(100vw, 100vh) * 4);
 		top: calc((0px - max(100vw, 100vh) * 2) + 20px);
 		right: calc((0px - max(100vw, 100vh) * 2) + 20px);
+		transition:
+			width 1s,
+			height 1s,
+			top 1s,
+			right 1s,
+			background-color 0s;
 	}
 </style>
