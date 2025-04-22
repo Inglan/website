5007c2cdd2ba7e16fc3e67b54247f90f81e1819e
Ingo Wolf <me@ingowolf.au>
2025-04-22 14:03:32 +1000

    use clsx to conditional open menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import clsx from 'clsx';
+
 	import Menu from '$lib/components/Menu.svelte';
 	import { menuOpen } from '$lib/state.svelte';
 </script>
@@ -9,7 +11,13 @@
 	class="bg-surface1/50 fixed top-5 right-5 flex h-15 w-15 items-center justify-center rounded-full backdrop-blur-lg duration-200 hover:scale-[1.1] active:scale-[0.95]"
 	id="menutrigger"
 >
-	<div class="group flex flex-col items-center justify-center gap-[3px]" id="menuicon">
+	<div
+		class={clsx(
+			'group menuicon flex flex-col items-center justify-center gap-[3px]',
+			menuOpen && 'open'
+		)}
+		id="menuicon"
+	>
 		<div
 			class="bg-text h-[2px] w-[18px] rounded-full duration-200 group-[.open]:translate-y-[5px] group-[.open]:rotate-45"
 		></div>
