aa5dff876a300a497b33bdcfb0b5357abd452841
Ingo Wolf <me@ingowolf.au>
2025-04-22 14:11:26 +1000

    add menubackground styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,13 @@
 	import { menuState } from '$lib/state.svelte';
 </script>
 
-<div class="fixed top-5 right-5 flex h-20 w-20 rounded-full backdrop-blur-lg" id="menu"></div>
+<div
+	class={clsx(
+		'fixed top-5 right-5 flex h-20 w-20 rounded-full backdrop-blur-lg duration-1000',
+		menuState.open && 'bg-surface1/50 open'
+	)}
+	id="menubackground"
+></div>
 
 <button
 	class="bg-surface1/50 fixed top-5 right-5 flex h-20 w-20 items-center justify-center rounded-full backdrop-blur-lg duration-200 hover:scale-[1.1] active:scale-[0.95]"
