e1f95df4aff831004480bc209810087453b600e6
Ingo Wolf <me@ingowolf.au>
2025-05-07 19:56:55 +1000

    xl round everything

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,23 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="flex flex-col gap-3 ">
         <div className="flex flex-row gap-3 grow">
-          <div className="flex bg-white rounded w-80 h-full"></div>
+          <div className="flex bg-white rounded-xl w-80 h-full"></div>
           <div className="flex flex-col gap-3 grow">
             <div className="flex flex-row gap-3 grow">
-              <div className="bg-white rounded w-64 h-40"></div>
-              <div className="bg-white rounded w-64 h-40"></div>
+              <div className="bg-white rounded-xl w-64 h-40"></div>
+              <div className="bg-white rounded-xl w-64 h-40"></div>
             </div>
             <div className="flex flex-row gap-3 grow">
-              <div className="bg-white rounded grow h-40"></div>
-              <div className="bg-white rounded w-56 h-40"></div>
+              <div className="bg-white rounded-xl grow h-40"></div>
+              <div className="bg-white rounded-xl w-56 h-40"></div>
             </div>
           </div>
         </div>
         <div className="flex flex-row gap-3 grow">
-          <div className="flex bg-white rounded grow h-32"></div>
-          <div className="flex bg-white rounded grow h-32"></div>
-          <div className="flex bg-white rounded grow h-32"></div>
-          <div className="flex bg-white rounded grow h-32"></div>
+          <div className="flex bg-white rounded-xl grow h-32"></div>
+          <div className="flex bg-white rounded-xl grow h-32"></div>
+          <div className="flex bg-white rounded-xl grow h-32"></div>
+          <div className="flex bg-white rounded-xl grow h-32"></div>
         </div>
       </div>
     </div>
