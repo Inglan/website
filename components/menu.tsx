f70ef055f634936204d4e17fd14bc2b9708cd7ea
Ingo Wolf <me@ingowolf.au>
2025-05-23 13:01:27 +1000

    start working on custom menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -13,29 +13,50 @@ import {
 } from "@/components/ui/drawer";
 import { Menu } from "lucide-react";
 import { useMenu } from "@/context/MenuContext";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function SiteMenu() {
   const { menuOpen, setMenuOpen, toggleMenu } = useMenu();
 
+  const menuContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (menuOpen && menuContainer.current) {
+      menuContainer.current.style.display = "flex";
+      gsap.to(menuContainer.current, {
+        opacity: 1,
+        duration: 0.4,
+        ease: "power2.out",
+      });
+    } else if (menuContainer.current) {
+      gsap.to(menuContainer.current, {
+        opacity: 0,
+        duration: 0.4,
+        ease: "power2.in",
+        onComplete: () => {
+          if (menuContainer.current) {
+            menuContainer.current.style.display = "none";
+          }
+        },
+      });
+    }
+  }, [menuOpen]);
+
   return (
     <>
       <Button
-        className="top-3 right-3 fixed"
+        className="top-3 right-3 fixed z-50"
         size="icon"
         variant="ghost"
         onClick={toggleMenu}
       >
         <Menu />
       </Button>
-      <Drawer
-        open={menuOpen}
-        onClose={() => setMenuOpen(false)}
-        direction="right"
-      >
-        <DrawerContent>
-          <DrawerTitle>Menu</DrawerTitle>
-        </DrawerContent>
-      </Drawer>
+      <div
+        ref={menuContainer}
+        className="fixed top-0 left-0 h-full w-full bg-surface0/50 z-40 justify-center items-center backdrop-blur-2xl opacity-0 hidden"
+      ></div>
     </>
   );
 }
