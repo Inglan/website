1d1cdbbe3a7d7115c64911580cf728c37f69ae74
Ingo Wolf <me@ingowolf.au>
2025-06-19 21:25:33 +1000

    move menu to own component

---
+++ src/lib/components/menu.svelte
@@ -0,0 +1,11 @@
+<div class="min-w-96 h-screen sticky top-0 left-0 flex justify-center p-4 flex-col gap-4">
+	<a href="/" class="text-7xl text-primary duration-100">Home</a>
+	<a href="/projects" class="text-7xl text-foreground/50 hover:text-foreground duration-100"
+		>Projects</a
+	>
+	<a href="/about" class="text-7xl text-foreground/50 hover:text-foreground duration-100">About</a>
+	<a href="/blog" class="text-7xl text-foreground/50 hover:text-foreground duration-100">Blog</a>
+	<a href="/contact" class="text-7xl text-foreground/50 hover:text-foreground duration-100"
+		>Contact</a
+	>
+</div>
