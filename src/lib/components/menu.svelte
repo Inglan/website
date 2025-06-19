ae4bdfd40216ef4f07c5304bc85ccdc08f6bd444
Ingo Wolf <me@ingowolf.au>
2025-06-19 21:26:02 +1000

    change menu padding

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,4 +1,4 @@
-<div class="min-w-96 h-screen sticky top-0 left-0 flex justify-center p-4 flex-col gap-4">
+<div class="min-w-96 h-screen sticky top-0 left-0 flex justify-center p-4 px-10 flex-col gap-4">
 	<a href="/" class="text-7xl text-primary duration-100">Home</a>
 	<a href="/projects" class="text-7xl text-foreground/50 hover:text-foreground duration-100"
 		>Projects</a
