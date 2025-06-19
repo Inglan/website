5e0aa6179c15792af10606685c307d3b47733776
Ingo Wolf <me@ingowolf.au>
2025-06-19 21:30:13 +1000

    simplify

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,11 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import clsx from 'clsx';
+</script>
+
+{#snippet menuItem(url: string, label: string)}
+	<a href={url} class="text-7xl text-foreground/50 hover:text-foreground duration-100">{label}</a>
+{/snippet}
+
 <div class="min-w-96 h-screen sticky top-0 left-0 flex justify-center p-4 px-10 flex-col gap-4">
-	<a href="/" class="text-7xl text-primary duration-100">Home</a>
-	<a href="/projects" class="text-7xl text-foreground/50 hover:text-foreground duration-100"
-		>Projects</a
-	>
-	<a href="/about" class="text-7xl text-foreground/50 hover:text-foreground duration-100">About</a>
-	<a href="/blog" class="text-7xl text-foreground/50 hover:text-foreground duration-100">Blog</a>
-	<a href="/contact" class="text-7xl text-foreground/50 hover:text-foreground duration-100"
-		>Contact</a
-	>
+	{@render menuItem('/', 'Home')}
+	{@render menuItem('/about', 'About')}
+	{@render menuItem('/projects', 'Projects')}
+	{@render menuItem('/blog', 'Blog')}
+	{@render menuItem('/contact', 'Contact')}
 </div>
