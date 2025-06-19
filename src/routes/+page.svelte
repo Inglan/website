49df5e677a2ee0aa75f41ad3b47145ca6b14eafb
Ingo Wolf <me@ingowolf.au>
2025-06-19 21:52:08 +1000

    add heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full h-screen p-3">
+	<div class="w-full h-full flex justify-center items-center">
+		<div class="flex flex-col gap-3">
+			<h1 class="text-8xl">Hi, I'm Ingo</h1>
+			<h2 class="text-4xl">And I build for the web</h2>
+		</div>
+	</div>
+</div>
