337564c89dc023c96055f0fc37777c55e388ddc2
Ingo Wolf <me@ingowolf.au>
2025-06-19 22:00:31 +1000

    add grid background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	import clsx from 'clsx';
+</script>
+
 <div class="w-full h-screen p-3">
-	<div class="w-full h-full flex justify-center items-center">
+	<div
+		class="w-full h-full flex justify-center items-center rounded-4xl [background-image:linear-gradient(to_right,#222222_1px,transparent_1px),linear-gradient(to_bottom,#222222_1px,transparent_1px)] [background-size:40px_40px] [background-position:20px_20px]"
+	>
 		<div class="flex flex-col gap-3">
 			<h1 class="text-8xl">Hi, I'm Ingo</h1>
 			<h2 class="text-4xl">And I build for the web</h2>
