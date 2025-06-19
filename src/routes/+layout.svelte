0d0309971d6de81d27bee3fd5598b119ea0452ad
Ingo Wolf <me@ingowolf.au>
2025-06-19 21:33:54 +1000

    remove padding from page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <div class="flex flex-row w-full">
 	<Menu />
 	<div class="w-full">
-		<main class="p-4 container mx-auto">
+		<main class="container mx-auto">
 			{@render children()}
 		</main>
 	</div>
