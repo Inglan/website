84f7f8446577e6c154182416758b0067a6f611f1
Ingo Wolf <me@ingowolf.au>
2025-06-20 14:10:00 +1000

    add contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,31 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 
-<div class="container m-auto p-3">
+	const id = $props.id();
+</script>
+
+<div class="container mx-auto p-3 flex flex-col gap-3">
 	<h1 class="text-6xl">Contact me</h1>
+	<form class="flex max-w-full w-3xl border rounded gap-3 flex-col p-3">
+		<div class="flex flex-row gap-3">
+			<div class="flex w-full max-w-sm flex-col gap-1.5">
+				<Label for="name-{id}">Name</Label>
+				<Input type="text" id="name-{id}" />
+			</div>
+			<div class="flex w-full max-w-sm flex-col gap-1.5">
+				<Label for="email-{id}">Email</Label>
+				<Input type="email" id="email-{id}" />
+			</div>
+		</div>
+
+		<div class="flex w-full flex-col gap-1.5">
+			<Label for="message">Your message</Label>
+			<Textarea class="resize-none h-72" placeholder="Type your message here." id="message" />
+		</div>
+
+		<Button type="submit" class="ml-auto">Send</Button>
+	</form>
 </div>
