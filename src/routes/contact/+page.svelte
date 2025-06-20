e5a45c61efee2056276c7454e10e05424674f16a
Ingo Wolf <me@ingowolf.au>
2025-06-20 14:29:30 +1000

    remove borders

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -13,7 +13,7 @@
 <div class="container mx-auto p-3 flex flex-col gap-3">
 	<h1 class="text-6xl">Contact me</h1>
 	<div class="flex md:flex-row gap-3 w-full flex-col">
-		<form class="flex w-full border rounded gap-3 flex-col p-3 border-dashed">
+		<form class="flex w-full rounded gap-3 flex-col p-3">
 			<div class="flex flex-row gap-3">
 				<div class="flex w-full max-w-sm flex-col gap-1.5">
 					<Label for="name-{id}">Name</Label>
@@ -33,7 +33,7 @@
 			<Button type="submit" class="ml-auto">Send</Button>
 		</form>
 		<div class="flex flex-col gap-3 w-full">
-			<div class="flex pl-10 justify-center h-full border border-dashed rounded flex-col gap-3">
+			<div class="flex pl-10 h-full rounded flex-col gap-3">
 				<a
 					href="mailto:me@ingo.au"
 					class="text-4xl hover:underline flex flex-row gap-2 items-center"
