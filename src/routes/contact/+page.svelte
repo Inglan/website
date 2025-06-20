fe64cb687d1f4b6d780f400f6c9a06fa11a94f6e
Ingo Wolf <me@ingowolf.au>
2025-06-20 14:11:20 +1000

    change styling

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,23 +9,26 @@
 
 <div class="container mx-auto p-3 flex flex-col gap-3">
 	<h1 class="text-6xl">Contact me</h1>
-	<form class="flex max-w-full w-3xl border rounded gap-3 flex-col p-3">
-		<div class="flex flex-row gap-3">
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="name-{id}">Name</Label>
-				<Input type="text" id="name-{id}" />
+	<div class="flex md:flex-row gap-3 w-full flex-col">
+		<form class="flex w-full border rounded gap-3 flex-col p-3">
+			<div class="flex flex-row gap-3">
+				<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<Label for="name-{id}">Name</Label>
+					<Input type="text" id="name-{id}" />
+				</div>
+				<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<Label for="email-{id}">Email</Label>
+					<Input type="email" id="email-{id}" />
+				</div>
 			</div>
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="email-{id}">Email</Label>
-				<Input type="email" id="email-{id}" />
+
+			<div class="flex w-full flex-col gap-1.5">
+				<Label for="message">Your message</Label>
+				<Textarea class="resize-none h-72" placeholder="Type your message here." id="message" />
 			</div>
-		</div>
 
-		<div class="flex w-full flex-col gap-1.5">
-			<Label for="message">Your message</Label>
-			<Textarea class="resize-none h-72" placeholder="Type your message here." id="message" />
-		</div>
-
-		<Button type="submit" class="ml-auto">Send</Button>
-	</form>
+			<Button type="submit" class="ml-auto">Send</Button>
+		</form>
+		<div class="flex flex-col gap-3 w-full">test</div>
+	</div>
 </div>
