5fe624ac8da6ad28d5fad8d0d23e91d5ac469868
Ingo Wolf <me@ingowolf.au>
2025-06-21 19:22:32 +1000

    change spacing

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,18 +6,19 @@
 	const { data }: PageProps = $props();
 </script>
 
-<div
-	class="w-full xl:max-w-6xl md:max-w-3xl max-w-lg mx-auto p-3 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3"
->
-	{#each data.posts as post}
-		<a href={post.slug} class="block h-full">
-			<Card.Root class="pt-0 h-full">
-				<img src={post.image} class="w-full aspect-video rounded object-cover" alt="" />
-				<Card.Header>
-					<Card.Title>{post.title}</Card.Title>
-					<Card.Description class="line-clamp-2">{post.description}</Card.Description>
-				</Card.Header>
-			</Card.Root>
-		</a>
-	{/each}
+<div class="w-full xl:max-w-6xl md:max-w-3xl max-w-lg mx-auto p-3 flex flex-col gap-5">
+	<h1 class="text-6xl">Blog</h1>
+	<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+		{#each data.posts as post}
+			<a href={post.slug} class="block h-full">
+				<Card.Root class="pt-0 h-full">
+					<img src={post.image} class="w-full aspect-video rounded object-cover" alt="" />
+					<Card.Header>
+						<Card.Title>{post.title}</Card.Title>
+						<Card.Description class="line-clamp-2">{post.description}</Card.Description>
+					</Card.Header>
+				</Card.Root>
+			</a>
+		{/each}
+	</div>
 </div>
