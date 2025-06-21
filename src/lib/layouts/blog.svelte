d091d2b7f71d2e7bd6680886a24685038006b94b
Ingo Wolf <me@ingowolf.au>
2025-06-21 20:01:13 +1000

    redesign header

--- src/lib/layouts/blog.svelte
+++ src/lib/layouts/blog.svelte
@@ -13,43 +13,45 @@
 	import IconChevronLeft from '~icons/material-symbols/chevron-left-rounded';
 </script>
 
-<div class="max-w-prose prose-lg md:prose-xl w-full px-3 pt-3 mx-auto">
-	<Button variant="outline" href="/blog">
-		<IconChevronLeft />
-		Back
-	</Button>
-</div>
-<div class="prose prose-lg md:prose-xl prose-invert mx-auto p-3">
-	<div class="flex min-h-96 pt-20 items-end relative p-5">
-		<img
-			src={image}
-			class="object-cover w-full h-full absolute top-0 left-0 -z-20 !m-0 rounded-t-xl"
-			alt=""
-		/>
-		<div class="flex flex-col md:flex-row gap-3 w-full h-fit justify-end md:items-end">
-			<div
-				class="absolute top-0 left-0 w-full h-full from-transparent to-background bg-gradient-to-b -z-10"
-			></div>
-			<h1 class="!m-0">{title}</h1>
-			<div class="grow"></div>
+<div class="flex min-h-96 relative p-5 flex-col">
+	<img
+		src={image}
+		class="object-cover w-full h-full absolute top-0 left-0 -z-20 opacity-50 md:opacity-100"
+		alt=""
+	/>
+	<div class="mx-auto w-full max-w-prose prose-lg md:prose-xl">
+		<Button variant="outline" class="bg-background/75 backdrop-blur-xl" href="/blog"
+			><IconChevronLeft />Back</Button
+		>
+	</div>
+	<div class="grow"></div>
+	<div
+		class="flex flex-col md:flex-row gap-3 w-full h-fit justify-end md:items-end max-w-prose prose-lg md:prose-xl mx-auto mt-10 md:p-3"
+	>
+		<div
+			class="absolute top-0 left-0 w-full h-full from-transparent to-background bg-gradient-to-b -z-10"
+		></div>
+		<h1 class="!m-0">{title}</h1>
+		<div class="grow"></div>
+		<div class="flex flex-col p-3 border border-dashed rounded-xl h-fit text-nowrap">
+			<div class="flex flex-row gap-1 items-center">
+				<IconCalendar class="size-6" />
+				{dayjs.unix(posted).format('D MMM YYYY')}
+			</div>
+			{dayjs.unix(posted).fromNow()}
+		</div>
+		{#if updated}
 			<div class="flex flex-col p-3 border border-dashed rounded-xl h-fit text-nowrap">
 				<div class="flex flex-row gap-1 items-center">
-					<IconCalendar class="size-6" />
-					{dayjs.unix(posted).format('D MMM YYYY')}
+					<IconEdit class="size-6" />
+					{dayjs.unix(updated).format('D MMM YYYY')}
 				</div>
-				{dayjs.unix(posted).fromNow()}
-			</div>
-			{#if updated}
-				<div class="flex flex-col p-3 border border-dashed rounded-xl h-fit text-nowrap">
-					<div class="flex flex-row gap-1 items-center">
-						<IconEdit class="size-6" />
-						{dayjs.unix(updated).format('D MMM YYYY')}
-					</div>
 
-					{dayjs.unix(updated).fromNow()}
-				</div>
-			{/if}
-		</div>
+				{dayjs.unix(updated).fromNow()}
+			</div>
+		{/if}
 	</div>
+</div>
+<div class="prose prose-lg md:prose-xl prose-invert mx-auto p-3">
 	{@render children()}
 </div>
