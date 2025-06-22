a78323ef2a56a8a53d9e123890c17b972d921347
Ingo Wolf <me@ingowolf.au>
2025-06-22 19:27:08 +1000

    add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,23 @@
 	import '../app.css';
 	import Menu from '$lib/components/menu.svelte';
 
+	import { page } from '$app/state';
+	import { fade, fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
+
 	let { children } = $props();
 </script>
 
 <div class="flex flex-row w-full">
 	<Menu />
 	<div class="w-full">
-		<main>
-			{@render children()}
-		</main>
+		{#key page.url}
+			<main
+				in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 300 }}
+				out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
+			>
+				{@render children()}
+			</main>
+		{/key}
 	</div>
 </div>
