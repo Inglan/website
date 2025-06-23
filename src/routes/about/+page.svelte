f48a36627c718bedfe6941727a0bcdeaf024eb8a
Ingo Wolf <me@ingowolf.au>
2025-06-23 13:22:28 +1000

    add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,15 @@
-<div class="container mx-auto p-5 flex flex-col gap-5">
+<div class="prose prose-lg md:prose-xl prose-invert mx-auto p-3">
 	<h1 class="text-6xl">About Me</h1>
+	I am a web developer but also know a bit of game development
+	<h2 class="text-4xl">I know</h2>
+	<ul>
+		<li>NodeJS</li>
+		<li>React</li>
+		<li>NextJS</li>
+		<li>Svelte</li>
+		<li>TypeScript</li>
+		<li>Godot</li>
+		<li>Python</li>
+		<li>Docker</li>
+	</ul>
 </div>
