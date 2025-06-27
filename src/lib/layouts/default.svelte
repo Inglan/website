9dceed2c06adaa3a7b189cf0488bc0044ad1ceaf
Ingo Wolf <me@ingowolf.au>
2025-06-27 14:12:11 +1000

    add content component

--- src/lib/layouts/default.svelte
+++ src/lib/layouts/default.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	let { children } = $props();
+	import Content from '$lib/components/content.svelte';
 </script>
 
-<div
-	class="prose prose-lg prose-custom md:prose-xl mx-auto p-3 prose-headings:font-normal prose-h1:text-6xl"
->
-	{@render children()}
-</div>
+<Content>{@render children()}</Content>
