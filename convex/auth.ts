f95cd155abd2575e88c1823babeec8d5973358ec
Ingo Wolf <me@ingowolf.au>
2025-08-16 19:46:48 +1000

    set role on signup

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,4 +3,13 @@ import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [GitHub],
+  callbacks: {
+    async afterUserCreatedOrUpdated(ctx, args) {
+      if (args.existingUserId) return;
+
+      await ctx.db.patch(args.userId, {
+        role: "read",
+      });
+    },
+  },
 });
