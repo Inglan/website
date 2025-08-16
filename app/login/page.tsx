72fa4ee31bb3872c08184395cbc9e8d312fc4cd6
Ingo Wolf <me@ingowolf.au>
2025-08-16 15:11:37 +1000

    add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect } from "react";
+import { LoaderCircle } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+export default async function Login() {
+  const { signIn } = useAuthActions();
+  useEffect(() => {
+    signIn("github");
+  });
+
+  return (
+    <div className="w-full h-full flex justify-center items-center gap-3">
+      <LoaderCircle className="animate-spin text-primary" />
+      Redirecting to GitHub
+    </div>
+  );
+}
