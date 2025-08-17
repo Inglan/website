3c91de6c27ca958b5da7f09dbc246bc2b129b2a0
Ingo Wolf <me@ingowolf.au>
2025-08-17 11:47:22 +1000

    add create mutation

--- convex/projects.ts
+++ convex/projects.ts
@@ -12,3 +12,19 @@ export const list = query({
     return projects;
   },
 });
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const slug = encodeURIComponent(
+      args.name.toLowerCase().replace(/\s+/g, "-"),
+    );
+    const id = await ctx.db.insert("projects", {
+      name: args.name,
+      slug,
+    });
+    return id;
+  },
+});
