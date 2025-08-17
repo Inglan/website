a6cfd0eec3186c372204172900f125022dbea753
Ingo Wolf <me@ingowolf.au>
2025-08-17 22:37:34 +1000

    save show on save

--- app/projects/[slug]/edit/editor.tsx
+++ app/projects/[slug]/edit/editor.tsx
@@ -187,6 +187,7 @@ export default function ProjectEditor(props: {
                 content,
                 tags,
                 links,
+                show,
               }),
               {
                 loading: "Saving...",
