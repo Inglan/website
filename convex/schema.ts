64e33a6f5040cd3cf041ba9e0cfb6f69cbcc86dc
Ingo Wolf <me@ingowolf.au>
2025-08-17 10:00:12 +1000

    add schema for posts and projects

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,6 +3,30 @@ import { authTables } from "@convex-dev/auth/server";
 import { v } from "convex/values";
 
 const schema = defineSchema({
+  projects: defineTable({
+    name: v.string(),
+    slug: v.string(),
+    description: v.string(),
+    content: v.string(),
+    links: v.array(
+      v.object({
+        title: v.string(),
+        url: v.string(),
+      }),
+    ),
+    tags: v.array(v.string()),
+    image: v.id("_storage"),
+    images: v.array(v.id("_storage")),
+  }),
+  posts: defineTable({
+    title: v.string(),
+    slug: v.string(),
+    content: v.string(),
+    tags: v.array(v.string()),
+    image: v.id("_storage"),
+    images: v.array(v.id("_storage")),
+  }),
+
   ...authTables,
   users: defineTable({
     name: v.optional(v.string()),
