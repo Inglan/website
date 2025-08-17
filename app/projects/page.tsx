7ef3819b0486d4e1997f0d0ab4fc89c25e3bbf30
Ingo Wolf <me@ingowolf.au>
2025-08-17 11:23:56 +1000

    add aniamte in

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,37 +15,52 @@ import Link from "next/link";
 import Content from "@/components/content";
 import { ChevronDown, LoaderCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { AnimatePresence } from "motion/react";
+import { AnimatePresence, stagger } from "motion/react";
 import { motion } from "motion/react";
+import { useState } from "react";
 
 export default function ProjectsPage() {
   const {
     results: projects,
     status,
     loadMore,
-  } = usePaginatedQuery(api.projects.list, {}, { initialNumItems: 5 });
+  } = usePaginatedQuery(api.projects.list, {}, { initialNumItems: 10 });
+  const [lastLoadedCount, setLastLoadedCount] = useState(0);
+
   return (
     <Content>
       <h1>Projects</h1>
-      <div className="not-prose flex gap-2 flex-col">
-        <div className="min-w-full grid grid-cols-1 lg:grid-cols-2 gap-2">
-          {projects.map((project) => (
-            <Link href={`/projects/${project.slug}`} key={project._id}>
-              <Card className="mb-2 h-full">
-                <CardHeader>
-                  <CardTitle>{project.name}</CardTitle>
-                  <CardDescription>{project.description}</CardDescription>
-                </CardHeader>
-              </Card>
-            </Link>
-          ))}
-        </div>
-        <div className="flex justify-center items-center h-full">
-          <AnimatePresence>
+      <AnimatePresence>
+        <div className="not-prose flex gap-2 flex-col">
+          <div className="min-w-full grid grid-cols-1 lg:grid-cols-2 gap-2">
+            {projects.map((project, index) => (
+              <motion.div
+                key={project._id}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 10 }}
+                transition={{
+                  ease: "easeOut",
+                  delay: (index - lastLoadedCount) * 0.1,
+                }}
+              >
+                <Link href={`/projects/${project.slug}`}>
+                  <Card className="mb-2 h-full">
+                    <CardHeader>
+                      <CardTitle>{project.name}</CardTitle>
+                      <CardDescription>{project.description}</CardDescription>
+                    </CardHeader>
+                  </Card>
+                </Link>
+              </motion.div>
+            ))}
+          </div>
+          <div className="flex justify-center items-center h-full">
             {status === "LoadingFirstPage" ? (
               <LoaderCircle className="animate-spin" />
             ) : (
               <motion.div
+                layout
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
@@ -53,6 +68,7 @@ export default function ProjectsPage() {
                 <Button
                   disabled={!(status === "CanLoadMore")}
                   onClick={() => {
+                    setLastLoadedCount(projects.length);
                     loadMore(10);
                   }}
                 >
@@ -65,9 +81,9 @@ export default function ProjectsPage() {
                 </Button>
               </motion.div>
             )}
-          </AnimatePresence>
+          </div>
         </div>
-      </div>
+      </AnimatePresence>
     </Content>
   );
 }
