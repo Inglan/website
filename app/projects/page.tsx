24f33c9f3f2764f4f095d888355b4cd9bd02d0da
Ingo Wolf <me@ingowolf.au>
2025-08-17 10:51:34 +1000

    add feature where it shwos stuff

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,19 +2,72 @@
 import { api } from "@/convex/_generated/api";
 import { usePaginatedQuery, usePreloadedQuery } from "convex/react";
 import { preloadQuery } from "convex/nextjs";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Content from "@/components/content";
+import { ChevronDown, LoaderCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 
 export default function ProjectsPage() {
   const {
     results: projects,
     status,
     loadMore,
-  } = usePaginatedQuery(api.projects.list, {}, { initialNumItems: 10 });
+  } = usePaginatedQuery(api.projects.list, {}, { initialNumItems: 1 });
   return (
-    <div>
+    <Content>
       <h1>Projects</h1>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
-    </div>
+      <div className="not-prose flex gap-2 flex-col">
+        <div className="grid lg:grid-cols-2 container min-w-full gap-2">
+          {projects.map((project) => (
+            <Link href={`/projects/${project.slug}`} key={project._id}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>{project.name}</CardTitle>
+                  <CardDescription>{project.description}</CardDescription>
+                </CardHeader>
+              </Card>
+            </Link>
+          ))}
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <AnimatePresence>
+            {status === "LoadingFirstPage" ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <Button
+                  disabled={!(status === "CanLoadMore")}
+                  onClick={() => {
+                    loadMore(10);
+                  }}
+                >
+                  {status === "LoadingMore" ? (
+                    <LoaderCircle className="animate-spin" />
+                  ) : (
+                    <ChevronDown />
+                  )}
+                  More
+                </Button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </div>
+    </Content>
   );
 }
