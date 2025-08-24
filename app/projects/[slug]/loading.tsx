7da93d6346af1764ca7a2e3c754a92783ad209eb
Ingo Wolf <me@ingowolf.au>
2025-08-24 17:56:16 +1000

    change skeleton behaviour

--- app/projects/[slug]/loading.tsx
+++ app/projects/[slug]/loading.tsx
@@ -13,7 +13,11 @@ export default function Loading() {
       </h1>
       <div className="space-y-4">
         {Array.from({ length: 10 }).map((_, index) => (
-          <Skeleton key={index} className="h-6 w-[full]" />
+          <Skeleton
+            key={index}
+            className="h-6"
+            style={{ width: 50 + Math.random() * 50 + "%" }}
+          />
         ))}
       </div>
     </Content>
