2ed36b502937d2c3c60b3d4163596bbab691a6d0
Ingo Wolf <me@ingowolf.au>
2025-08-24 17:52:44 +1000

    add breadcrumbs

--- app/projects/[slug]/content.tsx
+++ app/projects/[slug]/content.tsx
@@ -4,6 +4,14 @@ import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Preloaded, usePreloadedQuery, useQuery } from "convex/react";
 import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export function ProjectContent(props: {
   preloadedProject: Preloaded<typeof api.projects.get>;
@@ -12,16 +20,31 @@ export function ProjectContent(props: {
 
   const projectData = usePreloadedQuery(props.preloadedProject);
   return (
-    <Content>
-      {me?.role === "write" && (
-        <div className="flex flex-row gap-2 p-2 not-prose">
-          <Button asChild>
-            <Link href={`/projects/${projectData.slug}/edit`}>Edit</Link>
-          </Button>
-        </div>
-      )}
-      <h1>{projectData.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: projectData.contenthtml }} />
-    </Content>
+    <>
+      <Content>
+        <Breadcrumb className="not-prose">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/projects">Projects</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{projectData.name}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        {me?.role === "write" && (
+          <div className="flex flex-row gap-2 p-2 not-prose">
+            <Button asChild>
+              <Link href={`/projects/${projectData.slug}/edit`}>Edit</Link>
+            </Button>
+          </div>
+        )}
+        <h1>{projectData.name}</h1>
+        <div dangerouslySetInnerHTML={{ __html: projectData.contenthtml }} />
+      </Content>
+    </>
   );
 }
