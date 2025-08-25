d13e2cbd4af8598789228a4f5eaaa9fa765b6caf
Ingo Wolf <me@ingowolf.au>
2025-08-25 16:27:55 +1000

    add custom metadata

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,21 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
-export { metadata, viewport } from 'next-sanity/studio'
+export { viewport } from "next-sanity/studio";
+import { metadata as sanityMetadata } from "next-sanity/studio";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Studio",
+  ...sanityMetadata,
+};
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
