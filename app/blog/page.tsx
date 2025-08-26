35201e01c038fc7790b503b3476c2853d32b1e1a
Ingo Wolf <me@ingowolf.au>
2025-08-26 12:58:01 +1000

    move posts to page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { type SanityDocument } from "next-sanity";
 
 import { client } from "@/sanity/client";
+import Posts from "./posts";
 
 const POSTS_QUERY = `*[
   _type == "post"
@@ -10,22 +11,23 @@ const POSTS_QUERY = `*[
 
 const options = { next: { revalidate: 30 } };
 
-export default async function IndexPage() {
+export default async function PostsIndexPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Posts</h1>
-      <ul className="flex flex-col gap-y-4">
-        {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
-            <Link href={`/blog/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <Posts posts={posts} />
+    // <main className="container mx-auto min-h-screen max-w-3xl p-8">
+    //   <h1 className="text-4xl font-bold mb-8">Posts</h1>
+    //   <ul className="flex flex-col gap-y-4">
+    //     {posts.map((post) => (
+    //       <li className="hover:underline" key={post._id}>
+    //         <Link href={`/blog/${post.slug.current}`}>
+    //           <h2 className="text-xl font-semibold">{post.title}</h2>
+    //           <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+    //         </Link>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </main>
   );
 }
