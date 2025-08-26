c64a38d11c0c8b72d0bf0ecb3fdeac32d3ecdc38
Ingo Wolf <me@ingowolf.au>
2025-08-26 13:45:00 +1000

    add animation to content component

--- components/content.tsx
+++ components/content.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { motion } from "motion/react";
+
 export default function Content({ children }: { children: React.ReactNode }) {
   return (
-    <div className="prose prose-lg prose-custom md:prose-xl mx-auto p-3 prose-headings:font-normal">
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 10 }}
+      transition={{
+        ease: [0.165, 0.84, 0.44, 1.0],
+      }}
+      className="prose prose-lg prose-custom md:prose-xl mx-auto p-3 prose-headings:font-normal"
+    >
       {children}
-    </div>
+    </motion.div>
   );
 }
