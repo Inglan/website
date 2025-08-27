f0de0f82ffcf2d010c80f2a36fc5c052f2f2971c
Ingo Wolf <me@ingowolf.au>
2025-08-27 18:56:37 +1000

    use redirection from docs

--- sanity/schemaTypes/redirectType.ts
+++ sanity/schemaTypes/redirectType.ts
@@ -1,35 +1,52 @@
 import { ArrowRight } from "lucide-react";
-import { defineField, defineType } from "sanity";
+import { defineType, defineField, SlugRule } from "sanity";
+
+// Shared validation for our redirect slugs
+const slugValidator = (rule: SlugRule) =>
+  rule.required().custom((value) => {
+    if (!value || !value.current) return "Can't be blank";
+    if (!value.current.startsWith("/")) {
+      return "The path must start with a /";
+    }
+    return true;
+  });
 
 export const redirectType = defineType({
   name: "redirect",
   title: "Redirect",
   type: "document",
+  description: "Redirect for next.config.js",
   icon: ArrowRight,
   fields: [
     defineField({
-      name: "name",
-      type: "string",
+      name: "source",
+      type: "slug",
+      validation: (rule) => slugValidator(rule),
     }),
     defineField({
-      name: "from",
-      type: "text",
+      name: "destination",
+      type: "slug",
+      validation: (rule) => slugValidator(rule),
     }),
     defineField({
-      name: "to",
-      type: "text",
+      name: "permanent",
+      type: "boolean",
     }),
   ],
+  // null / false makes it temporary (307)
+  initialValue: {
+    permanent: true,
+  },
   preview: {
     select: {
-      title: "name",
-      from: "from",
-      to: "to",
+      source: "source.current",
+      destination: "destination.current",
+      permanent: "permanent",
     },
-    prepare({ title, from, to }) {
+    prepare({ source, destination, permanent }) {
       return {
-        title: `Redirect from ${from} to ${to}`,
-        subtitle: title,
+        title: `Redirect ${source} to ${destination}`,
+        subtitle: permanent ? "Permanent" : "Temporary",
       };
     },
   },
