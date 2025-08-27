564dcf4899c5799691db7e32537cf1e2bb6da54a
Ingo Wolf <me@ingowolf.au>
2025-08-27 18:59:07 +1000

    add redirects to next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import { client } from "./sanity/lib/client";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  async redirects() {
+    const redirects = await client.fetch(
+      `*[_type == "redirect"]{
+        "source":source.current,
+        "destination":destination.current,
+        permanent
+      }`,
+    );
+    return redirects;
+  },
 };
 
 const withMDX = createMDX({
