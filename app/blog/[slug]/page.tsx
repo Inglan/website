6f29ee55345be603cbbf3f38677e518ee1c9f6e4
Ingo Wolf <me@ingowolf.au>
2025-08-30 18:29:36 +1000

    add more padding

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -46,7 +46,7 @@ export default async function PostPage({
             />
           )}
           <div className="w-full h-full absolute top-0 left-0 from-transparent to-background bg-gradient-to-b"></div>
-          <div className="w-full min-h-72 p-2 flex flex-col relative z-20">
+          <div className="w-full min-h-72 p-4 flex flex-col relative z-20">
             <div className="flex flex-row gap-2">
               <Button variant="secondary" asChild>
                 <Link href="/blog">
