1dbe82e98af550cfe46c3a3ddce1e696e1d3d0f8
Ingo Wolf <me@ingowolf.au>
2025-09-03 19:47:53 +1000

    add redirect middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,23 @@
 import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
+import { client } from "./sanity/lib/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const redirect = await client.fetch(
+    `*[_type == "redirect" && permanent == true && source.current == $source ]{
+        "source": source.current,
+        "destination": destination,
+        permanent
+      }[0]`,
+    { source: pathname },
+  );
+  if (redirect)
+    return NextResponse.redirect(
+      redirect.destination,
+      redirect.permanent ? 308 : 307,
+    );
+}
 
 export default convexAuthNextjsMiddleware();
 
