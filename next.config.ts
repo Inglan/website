9e18edacc286d816dc76e931ad945599e69145fc
Ingo Wolf <me@ingowolf.au>
2025-09-03 19:46:49 +1000

    delete redirecsts from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,6 @@ import { client } from "./sanity/lib/client";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  async redirects() {
-    const redirects = await client.fetch(
-      `*[_type == "redirect"]{
-        "source":source.current,
-        "destination":destination.current,
-        permanent
-      }`,
-    );
-    return redirects;
-  },
 };
 
 const withMDX = createMDX({
