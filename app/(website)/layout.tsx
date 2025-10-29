cd98658b7ddd71c5af9734d19c9ac029fae920ed
Ingo Wolf <me@ingowolf.au>
2025-10-29 19:37:02 +1100

    init sanity

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -17,12 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistMono.className} antialiased`}>
-        <ThemeProvider attribute="class" forcedTheme="dark">
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <div
+      vaul-drawer-wrapper=""
+      className="flex flex-row w-full bg-background min-h-screen"
+    >
+      <Navbar />
+      {children}
+    </div>
   );
 }
