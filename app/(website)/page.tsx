538fff58011fc7b5515bed1d5226958a9c70fbb2
Ingo Wolf <me@ingowolf.au>
2025-10-29 20:18:16 +1100

    chnage more layout stuff

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full mx-auto flex flex-col items-center justify-center h-fit p-2 py-16 border-b border-dashed">
-      <div className="flex flex-col">
+    <div className="w-full mx-auto flex flex-row items-center justify-center h-fit p-2 py-0 border-b border-dashed">
+      <div className="flex flex-col py-32">
         <div className="text-2xl">Hi, I&apos;m</div>
         <div className="lg:text-8xl text-7xl">Ingo Wolf</div>
       </div>
