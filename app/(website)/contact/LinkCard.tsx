ff1059e842f367f8d86dc822719bb394908bf740
Ingo Wolf <me@ingowolf.au>
2025-10-29 21:23:33 +1100

    add animations

--- app/(website)/contact/LinkCard.tsx
+++ app/(website)/contact/LinkCard.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { motion } from "motion/react";
+import { NICE_EASE } from "@/lib/constants";
 
 interface LinkCardProps {
   href: string;
@@ -13,12 +14,19 @@ interface LinkCardProps {
 
 export default function LinkCard({ href, children, colSpan }: LinkCardProps) {
   return (
-    <motion.div className={clsx(colSpan && `col-span-${colSpan}`)}>
+    <motion.div
+      className={clsx(colSpan && `col-span-${colSpan}`)}
+      initial={{ backgroundColor: "var(--background)" }}
+      animate={{ backgroundColor: "var(--background)" }}
+      whileHover={{ backgroundColor: "var(--card)" }}
+      whileTap={{ filter: "brightness(0.7)" }}
+      transition={{ ease: NICE_EASE }}
+    >
       <Link
         href={href}
         target="_blank"
         rel="noopener noreferrer"
-        className="p-4 border-b border-r border-dashed hover:bg-card flex flex-col"
+        className="p-4 border-b border-r border-dashed flex flex-col"
       >
         {children}
       </Link>
