d6998b3622e873b758c08083a1f0fa51af83b488
Ingo Wolf <me@ingowolf.au>
2025-10-29 17:50:20 +1100

    add homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="w-full mx-auto flex flex-col items-center justify-center h-fit py-52">
+      <div className="flex flex-col">
+        <div className="text-2xl">Hi, I&apos;m</div>
+        <div className="text-8xl">Ingo Wolf</div>
+      </div>
+    </div>
+  );
 }
