05766561a5cfba5cbf1b80409990b29c1e90c863
Ingo Wolf <me@ingowolf.au>
2025-10-29 20:15:06 +1100

    change hero style

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="w-full mx-auto flex flex-col items-center justify-center h-fit py-52">
+    <div className="w-full mx-auto flex flex-col items-center justify-center h-fit p-2 py-16 border-b border-dashed">
       <div className="flex flex-col">
         <div className="text-2xl">Hi, I&apos;m</div>
-        <div className="text-8xl">Ingo Wolf</div>
+        <div className="lg:text-8xl text-7xl">Ingo Wolf</div>
       </div>
     </div>
   );
