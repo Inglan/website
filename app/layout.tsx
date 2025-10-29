ab102dcd1b85979b8cc8e149caed1048edc27444
Ingo Wolf <me@ingowolf.au>
2025-10-29 13:44:42 +1100

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,7 +21,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistMono.className} antialiased`}>
         <ThemeProvider attribute="class" forcedTheme="dark">
-          {children}
+          <div
+            vaul-drawer-wrapper=""
+            className="flex flex-row w-full bg-background min-h-screen"
+          >
+            <Navbar />
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
