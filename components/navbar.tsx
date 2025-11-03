674ebaf1597f654b10738e4fae70bc6197539820
Ingo Wolf <me@ingowolf.au>
2025-11-03 16:57:15 +1100

    add new navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import { motion } from "motion/react";
+import clsx from "clsx";
+
+export default function Navbar() {
+  return (
+    <div className="w-full p-4 pb-0">
+      <nav className="w-full max-w-4xl mx-auto border border-dashed flex flex-row">
+        <MenuLink href="/" className="border-l-0 border-r">
+          Ingo&apos;s Website
+        </MenuLink>
+        <div className="grow"></div>
+        <MenuLink href="/">Home</MenuLink>
+        <MenuLink href="/projects">Projects</MenuLink>
+        <MenuLink href="/blog">Blog</MenuLink>
+        <MenuLink href="/contact">Contact</MenuLink>
+      </nav>
+    </div>
+  );
+}
+
+function MenuLink({
+  children,
+  href,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}) {
+  return (
+    <Link href={href}>
+      <motion.div
+        className={clsx(
+          "border-l border-dashed h-full px-8 py-4 block w-fit",
+          className,
+        )}
+      >
+        {children}
+      </motion.div>
+    </Link>
+  );
+}
