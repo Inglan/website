845997ea1ce2cde3331d159c6b1addbc1442ff5e
Ingo Wolf <me@ingowolf.au>
2025-11-03 16:58:43 +1100

    update home page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full mx-auto flex flex-row items-center justify-center h-fit p-2 py-0 border-b border-dashed">
-      <div className="flex flex-col py-32">
+    <div className="w-full mx-auto flex flex-row items-center justify-center h-fit px-10 border border-t-0 border-dashed max-w-4xl">
+      <div className="flex flex-col py-32 w-full">
         <div className="text-2xl">Hi, I&apos;m</div>
         <div className="lg:text-8xl text-7xl">Ingo Wolf</div>
       </div>
