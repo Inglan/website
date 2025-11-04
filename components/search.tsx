003d745c449abff2365c21fc169ebecfe006a88b
Ingo Wolf <me@ingowolf.au>
2025-11-04 18:01:42 +1100

    add some commands to menu

--- components/search.tsx
+++ components/search.tsx
@@ -13,11 +13,39 @@ import {
 import { MENU_ITEMS } from "@/lib/constants";
 import { useUiState } from "@/lib/state";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Search() {
   const searchOpen = useUiState((state) => state.searchOpen);
   const setSearchOpen = useUiState((state) => state.setSearchOpen);
   const router = useRouter();
+
+  const commands = [
+    {
+      label: "Copy Email",
+      onSelect: () => {
+        navigator.clipboard.writeText("me@ingo.au");
+        setSearchOpen(false);
+        toast.success("Copied");
+      },
+    },
+    {
+      label: "Email",
+      onSelect: () => {
+        location.href = "mailto:me@ingo.au";
+        setSearchOpen(false);
+      },
+    },
+    {
+      label: "Signal",
+      onSelect: () => {
+        window.open(
+          "https://signal.me/#eu/aON_wvkns7bfeU-UAj_09B1Yym8WVC2QIWWV8rIhYZzPc2xGLUtBeLWMc9LJoWNB",
+        );
+        setSearchOpen(false);
+      },
+    },
+  ];
 
   return (
     <>
@@ -40,6 +68,13 @@ export default function Search() {
                 }}
               >
                 {item.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+          <CommandGroup heading="Commands">
+            {commands.map((command) => (
+              <CommandItem key={command.label} onSelect={command.onSelect}>
+                {command.label}
               </CommandItem>
             ))}
           </CommandGroup>
