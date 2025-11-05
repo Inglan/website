048895d068922761457b86b8bf24a69ef31228f6
Ingo Wolf <me@ingowolf.au>
2025-11-05 23:19:18 +1100

    add animation

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -14,7 +14,7 @@ export default function PostCard({
   return (
     <Link
       href={`/blog/${slug}`}
-      className="border-b border-dashed p-4 flex flex-col last:border-b-0"
+      className="border-b border-dashed p-4 flex flex-col last:border-b-0 hover:bg-card active:brightness-75 duration-300 ease-out"
     >
       <h1 className="text-2xl">{title}</h1>
       <p className="text-foreground/75">{date}</p>
