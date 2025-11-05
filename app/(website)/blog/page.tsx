a9aaba182dff8a5390cf80e6fcfd565510548270
Ingo Wolf <me@ingowolf.au>
2025-11-06 10:16:58 +1100

    add showing posts

--- app/(website)/blog/page.tsx
+++ app/(website)/blog/page.tsx
@@ -1,19 +1,32 @@
 import PostCard from "@/components/post-card";
+import { client } from "@/sanity/lib/client";
+import { SanityDocument } from "next-sanity";
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt, body}`;
+const options = { next: { revalidate: 30 } };
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+
   return (
     <div className="max-w-4xl w-full mx-auto border border-t-0 border-dashed">
-      {Array(10)
-        .fill(null)
-        .map((_, index) => (
-          <PostCard
-            key={index}
-            title="Test article"
-            slug="test-article"
-            date="06/07/1969"
-            excerpt="Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article"
-          />
-        ))}
+      {posts.map((post, index) => (
+        <PostCard
+          key={index}
+          title={post.title}
+          slug={post.slug.current}
+          date={post.publishedAt}
+          excerpt={post.body
+            .map((b: { children?: { text?: string }[] }) =>
+              (b.children || [])
+                .map((c: { text?: string }) => c.text || "")
+                .join(""),
+            )
+            .join("")}
+        />
+      ))}
     </div>
   );
 }
