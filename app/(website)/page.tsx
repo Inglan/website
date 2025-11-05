c314727ede605681f13cdd3375fe892bf721a37a
Ingo Wolf <me@ingowolf.au>
2025-11-05 20:00:25 +1100

    start working on homepage

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="w-full mx-auto flex flex-row items-center justify-center h-fit px-10 border border-t-0 border-dashed max-w-4xl">
-      <div className="flex flex-col py-32 w-full">
-        <div className="text-2xl">Hi, I&apos;m</div>
-        <div className="lg:text-8xl text-7xl">Ingo Wolf</div>
+    <div className="w-full mx-auto flex flex-row h-fit border border-t-0 border-dashed max-w-4xl">
+      <div className="w-52 border-r border-dashed">
+        <Image
+          width={255}
+          height={255}
+          alt=""
+          className="w-full aspect-square border-b border-dashed"
+          src="/icons/main.svg"
+        />
+        <div className="p-4">
+          <h1 className="text-2xl">Ingo Wolf</h1>
+        </div>
       </div>
+      <div className="p-4"></div>
     </div>
   );
 }
