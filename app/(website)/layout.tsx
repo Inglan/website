8c7c0ad5eda71eb441866a7a90a058c6defb2576
Ingo Wolf <me@ingowolf.au>
2025-11-05 21:32:04 +1100

    add footer

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -6,6 +6,7 @@ import Search from "@/components/search";
 import Navbar from "@/components/navbar";
 import Hotkeys from "@/lib/hotkeys";
 import { Toaster } from "@/components/ui/sonner";
+import Link from "@/components/custom-link";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -25,6 +26,13 @@ export default function RootLayout({
         {/*<Sidebar />*/}
         <Navbar />
         {children}
+        <div className="grow"></div>
+        <div className="max-w-4xl w-full mx-auto text-sm text-center text-foreground/75">
+          This website is{" "}
+          <Link href="https://github.com/Inglan/website" target="_blank">
+            open source
+          </Link>
+        </div>
       </div>
       <Toaster position="top-right" offset={{ top: 16, right: 16 }} />
       <Hotkeys />
