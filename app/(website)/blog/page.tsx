608efa5c88fb76fce9af4f06047205e8bfae2b22
Ingo Wolf <me@ingowolf.au>
2025-11-05 23:18:16 +1100

    add posts to post page

--- app/(website)/blog/page.tsx
+++ app/(website)/blog/page.tsx
@@ -1,3 +1,19 @@
+import PostCard from "@/components/post-card";
+
 export default function Blog() {
-  return <div></div>;
+  return (
+    <div className="max-w-4xl w-full mx-auto border border-t-0 border-dashed">
+      {Array(10)
+        .fill(null)
+        .map((_, index) => (
+          <PostCard
+            key={index}
+            title="Test article"
+            slug="test-article"
+            date="06/07/1969"
+            excerpt="Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article Test article"
+          />
+        ))}
+    </div>
+  );
 }
---
+++ components/post-card.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function PostCard({
+  title,
+  slug,
+  date,
+  excerpt,
+}: {
+  title: string;
+  slug: string;
+  date: string;
+  excerpt: string;
+}) {
+  return (
+    <Link
+      href={`/blog/${slug}`}
+      className="border-b border-dashed p-4 flex flex-col last:border-b-0"
+    >
+      <h1 className="text-2xl">{title}</h1>
+      <p className="text-foreground/75">{date}</p>
+      <p className="line-clamp-3">{excerpt}</p>
+    </Link>
+  );
+}
