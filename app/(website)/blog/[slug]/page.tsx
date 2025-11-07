8ce83490c441857c81956879a2e6d21f342f372d
Ingo Wolf <me@ingowolf.au>
2025-11-07 21:40:52 +1100

    add post rendering

--- app/(website)/blog/[slug]/page.tsx
+++ app/(website)/blog/[slug]/page.tsx
@@ -1,7 +1,38 @@
-export default function BlogPostPage() {
+import {
+  FilteredResponseQueryOptions,
+  PortableText,
+  type SanityDocument,
+} from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import { PostNavbar } from "@/components/navbar";
+
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
+
+const options: FilteredResponseQueryOptions = {
+  next: { revalidate: 30 },
+};
+
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const post = await client.fetch<SanityDocument>(
+    POST_QUERY,
+    await params,
+    options,
+  );
+
   return (
     <>
-      <></>
+      <PostNavbar title={post.title} />
+      <article className="max-w-4xl w-full mx-auto border border-t-0 border-dashed p-4">
+        <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+        <div className="prose">
+          <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+          {Array.isArray(post.body) && <PortableText value={post.body} />}
+        </div>
+      </article>
     </>
   );
 }
