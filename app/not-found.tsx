6a68d0e8f5462487833a23828c0337ff411c2e42
Ingo Wolf <me@ingowolf.au>
2025-11-15 17:35:09 +1100

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navbar />
+      <div className="w-full max-w-4xl mx-auto border-x border-b border-dashed p-10 flex flex-col items-center justify-center">
+        <h1 className="text-9xl">404</h1>
+        <p className="text-2xl">Page not found</p>
+      </div>
+    </div>
+  );
+}
