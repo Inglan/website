4fcbfb5d20d741514e282a9f981f88eadab8c7b5
Ingo Wolf <me@ingowolf.au>
2025-11-15 17:14:42 +1100

    add colors

---
+++ tailwind.config.ts
@@ -0,0 +1,45 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: "var(--color-foreground)",
+            a: {
+              color: "var(--color-primary)",
+            },
+            h1: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            h2: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            h3: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            h4: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            h5: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            h6: {
+              color: "var(--color-primary)",
+              fontWeight: "bold",
+            },
+            strong: {
+              color: "var(--color-foreground)",
+            },
+          },
+        },
+      },
+    },
+  },
+} satisfies Config;
