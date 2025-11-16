3d82f00ee190e41d86236f7d9054f3aa9d72e729
Ingo Wolf <me@ingowolf.au>
2025-11-16 16:21:02 +1100

    add share button

---
+++ components/post-share.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Check, Share, TriangleAlert } from "lucide-react";
+import { Button } from "./ui/button";
+import { SanityDocument } from "sanity";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function PostShare({ post }: { post: SanityDocument }) {
+  const [success, setSuccess] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [error, setError] = useState(false);
+
+  return (
+    <div>
+      <Button
+        variant="ghost"
+        className="border-l border-dashed"
+        onClick={() => {
+          setTimeout(() => {
+            setSuccess(false);
+            setCopied(false);
+            setError(false);
+          }, 2000);
+          if (!navigator.canShare || !navigator.share) {
+            if (!navigator.clipboard) {
+              setError(true);
+              toast.error("Failed to share");
+              return;
+            }
+            navigator.clipboard.writeText(location.href).then(() => {
+              setCopied(true);
+            });
+          } else {
+            navigator
+              .share({
+                url: location.href,
+              })
+              .then(() => {
+                setSuccess(true);
+              })
+              .catch(() => {
+                setError(true);
+                toast.error("Failed to share");
+              });
+          }
+        }}
+      >
+        {success ? (
+          <Check />
+        ) : error ? (
+          <TriangleAlert className="text-red-400" />
+        ) : (
+          <Share />
+        )}
+        Share
+      </Button>
+    </div>
+  );
+}
