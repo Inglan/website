8991d256901685a523eaceaee6ca2b4b1c3f1cb4
Ingo Wolf <me@ingowolf.au>
2025-11-22 17:36:47 +1100

    add webring thing

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from "@/components/navbar";
 import Hotkeys from "@/lib/hotkeys";
 import { Toaster } from "@/components/ui/sonner";
 import Link from "@/components/custom-link";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -23,6 +25,25 @@ export default function RootLayout({
         {children}
         <div className="grow"></div>
         <div className="max-w-4xl w-full mx-auto text-sm text-center text-foreground/75 mt-4">
+          <div className="flex flex-row w-fit items-center  mx-auto">
+            <Button variant="ghost" size="icon" asChild>
+              <Link
+                href="https://ctp-webr.ing/ingo/previous"
+                className="aspect-square h-full"
+              >
+                <ArrowLeft />
+              </Link>
+            </Button>
+            <Button variant="outline" className="border-dashed" asChild>
+              <Link href="https://ctp-webr.ing/">Catppuccin webring</Link>
+            </Button>
+            <Button variant="ghost" size="icon" asChild>
+              <Link href="https://ctp-webr.ing/ingo/next">
+                <ArrowRight />
+              </Link>
+            </Button>
+          </div>
+          <br />
           This website is{" "}
           <Link href="https://github.com/Inglan/website" target="_blank">
             open source
