fac97f935f2d0527e1c5bd16fbafe4637023f790
Ingo Wolf <me@ingowolf.au>
2025-11-24 09:06:32 +1100

    identify users

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -6,4 +6,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   defaults: "2025-05-24",
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking
   debug: process.env.NODE_ENV === "development",
+  person_profiles: "always",
 });
