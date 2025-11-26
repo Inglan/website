cb80e5d724bfcee9cf547bad46eb579cbf8ee2ed
Ingo Wolf <me@ingowolf.au>
2025-11-26 14:08:32 +1100

    dont use posthog in dev

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,15 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/a",
-  ui_host: "https://us.posthog.com",
-  defaults: "2025-05-24",
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-  debug: process.env.NODE_ENV === "development",
-  person_profiles: "always",
-});
+if (
+  !(process.env.NODE_ENV === "development") ||
+  process.env.ENABLE_INSTRUMENTATION === "true"
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: "/a",
+    ui_host: "https://us.posthog.com",
+    defaults: "2025-05-24",
+    capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+    debug: process.env.NODE_ENV === "development",
+    person_profiles: "always",
+  });
+}
