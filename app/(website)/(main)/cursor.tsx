8005a0fb5ed7d9dc699d68f939078a48c6dfab8c
Ingo Wolf <me@ingowolf.au>
2025-12-16 11:38:03 +1100

    remove cursor thing

--- app/(website)/(main)/cursor.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Cursor() {
-  try {
-    const { Cursor } = await import("motion-plus/react");
-    return <Cursor />;
-  } catch {
-    return <></>;
-  }
-}
--- app/(website)/(main)/layout.tsx
+++ app/(website)/(main)/layout.tsx
@@ -1,11 +1,9 @@
 import Navbar from "@/components/navbar";
-import Cursor from "./cursor";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <Cursor />
       {children}
     </>
   );
