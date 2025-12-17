574a1f175ebe61119bf1e5327bfa20ce5bd3e49c
Ingo Wolf <me@ingowolf.au>
2025-12-18 10:12:54 +1100

    add buttons to not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,37 @@
+"use client";
+
 import { Footer } from "@/components/footer";
 import Navbar from "@/components/navbar";
+import { useUiState } from "@/lib/state";
+import Link from "next/link";
 
 export default function NotFound() {
+  const setSearchOpen = useUiState((state) => state.setSearchOpen);
+
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="w-full max-w-4xl mx-auto border-x border-b border-dashed p-10 flex flex-col items-center justify-center">
-        <h1 className="text-9xl">404</h1>
-        <p className="text-2xl">Page not found</p>
+      <div className="w-full max-w-4xl mx-auto border-x border-b border-dashed py-20 flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col items-center">
+          <h1 className="text-9xl">404</h1>
+          <p className="text-2xl">Page not found</p>
+        </div>
+        <div className="w-full border-y border-dashed flex justify-center">
+          <Link
+            href="/"
+            className="px-8 py-4 border-l border-dashed duration-200 ease-out hover:bg-card active:brightness-75"
+          >
+            Home
+          </Link>
+          <button
+            onClick={() => setSearchOpen(true)}
+            className="cursor-pointer px-8 py-4 border-x border-dashed duration-200 ease-out hover:bg-card active:brightness-75"
+          >
+            Search
+          </button>
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
