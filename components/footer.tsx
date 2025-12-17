622347acc01a8d6d24615dbc6c2d0c8c601c10a6
Ingo Wolf <me@ingowolf.au>
2025-12-18 10:22:58 +1100

    responsiveness :)

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ import { MENU_ITEMS, OTHER_PAGES } from "@/lib/constants";
 
 export function Footer() {
   return (
-    <div className="max-w-4xl w-full mx-auto text-sm border-b border-l border-dashed flex flex-row">
+    <div className="max-w-4xl w-full mx-auto text-sm sm:border-b border-l border-dashed flex flex-col sm:flex-row">
       <div className="flex flex-col border-r border-dashed col-span-3">
         <NextLink
           href="/"
@@ -50,9 +50,9 @@ export function Footer() {
           </NextLink>
           <div className="border-b border-dashed grow bg-striped-gradient bg-size-[80px_80px] bg-fixed min-w-20"></div>
         </div>
-        <div className="h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10"></div>
+        <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10"></div>
       </div>
-      <div className="bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed grow"></div>
+      <div className="hidden sm:block bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed grow"></div>
       <div className="flex flex-col min-w-64">
         <div className="flex-col grid grid-cols-2 h-fit">
           {[...MENU_ITEMS, ...OTHER_PAGES].map((item) => (
@@ -65,7 +65,7 @@ export function Footer() {
             </NextLink>
           ))}
         </div>
-        <div className="h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10 border-r border-dashed"></div>
+        <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10 border-r border-dashed"></div>
       </div>
       {/*<div className="flex flex-row w-fit items-center  mx-auto">
         <Button variant="ghost" size="icon" asChild>
