353db6b2c2fffdcd72a6528c086780dc1e3075cb
Ingo Wolf <me@ingowolf.au>
2025-12-17 18:36:39 +1100

    add seperator to layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,6 +2,7 @@ import Search from "@/components/search";
 import Hotkeys from "@/lib/hotkeys";
 import { Toaster } from "@/components/ui/sonner";
 import { Footer } from "@/components/footer";
+import { StripedSeparator } from "@/components/striped-separator";
 
 export default function RootLayout({
   children,
@@ -15,6 +16,7 @@ export default function RootLayout({
         className="flex flex-col w-full bg-background min-h-screen p-4 pt-0"
       >
         {children}
+        <StripedSeparator />
         <Footer />
       </div>
       <Toaster position="top-right" offset={{ top: 16, right: 16 }} />
