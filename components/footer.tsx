e2634a1ac6d03d51cf0084b581e6f5ff3a02950c
Ingo Wolf <me@ingowolf.au>
2025-12-18 10:17:22 +1100

    change more sizing

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ import { MENU_ITEMS, OTHER_PAGES } from "@/lib/constants";
 export function Footer() {
   return (
     <div className="max-w-4xl w-full mx-auto text-sm border-b border-l border-dashed grid grid-cols-9">
-      <div className="flex flex-col border-r border-dashed col-span-4">
+      <div className="flex flex-col border-r border-dashed col-span-3">
         <NextLink
           href="/"
           className="flex text-xl p-4 border-b border-dashed active:font-normal font-extrabold font-mono duration-200 ease-out hover:bg-card active:brightness-75"
@@ -52,7 +52,7 @@ export function Footer() {
         </div>
         <div className="h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10"></div>
       </div>
-      <div className="bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed col-span-2"></div>
+      <div className="bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed col-span-3"></div>
       <div className="flex flex-col col-span-3">
         <div className="flex-col grid grid-cols-2 h-fit">
           {[...MENU_ITEMS, ...OTHER_PAGES].map((item) => (
