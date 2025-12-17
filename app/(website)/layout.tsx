bf609c63faef019fe383678fe55a3a2df2b0624a
Ingo Wolf <me@ingowolf.au>
2025-12-17 18:05:30 +1100

    move footer to component

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -24,37 +24,43 @@ export default function RootLayout({
       >
         {children}
         <div className="grow"></div>
-        <div className="max-w-4xl w-full mx-auto text-sm text-center text-foreground/75 mt-4">
-          <div className="flex flex-row w-fit items-center  mx-auto">
-            <Button variant="ghost" size="icon" asChild>
-              <Link
-                href="https://ctp-webr.ing/ingo/previous"
-                className="aspect-square h-full"
-              >
-                <span className="sr-only">Previous</span>
-                <ArrowLeft />
-              </Link>
-            </Button>
-            <Button variant="outline" className="border-dashed" asChild>
-              <Link href="https://ctp-webr.ing/">Catppuccin webring</Link>
-            </Button>
-            <Button variant="ghost" size="icon" asChild>
-              <Link href="https://ctp-webr.ing/ingo/next">
-                <span className="sr-only">Next</span>
-                <ArrowRight />
-              </Link>
-            </Button>
-          </div>
-          <br />
-          This website is{" "}
-          <Link href="https://github.com/Inglan/website" target="_blank">
-            open source
-          </Link>
-        </div>
+        <Footer />
       </div>
       <Toaster position="top-right" offset={{ top: 16, right: 16 }} />
       <Hotkeys />
       <Search />
     </>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="max-w-4xl w-full mx-auto text-sm text-center text-foreground/75 mt-4">
+      <div className="flex flex-row w-fit items-center  mx-auto">
+        <Button variant="ghost" size="icon" asChild>
+          <Link
+            href="https://ctp-webr.ing/ingo/previous"
+            className="aspect-square h-full"
+          >
+            <span className="sr-only">Previous</span>
+            <ArrowLeft />
+          </Link>
+        </Button>
+        <Button variant="outline" className="border-dashed" asChild>
+          <Link href="https://ctp-webr.ing/">Catppuccin webring</Link>
+        </Button>
+        <Button variant="ghost" size="icon" asChild>
+          <Link href="https://ctp-webr.ing/ingo/next">
+            <span className="sr-only">Next</span>
+            <ArrowRight />
+          </Link>
+        </Button>
+      </div>
+      <br />
+      This website is{" "}
+      <Link href="https://github.com/Inglan/website" target="_blank">
+        open source
+      </Link>
+    </div>
   );
 }
