d89189a55bd2fc31f87777a5150d0d88ba8bbfb3
Ingo Wolf <me@ingowolf.au>
2025-12-17 18:42:52 +1100

    change some styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,39 +7,43 @@ import { SiSignal } from "react-icons/si";
 
 export function Footer() {
   return (
-    <div className="max-w-4xl w-full mx-auto text-sm border-b border-x border-dashed grid grid-cols-4">
-      <div className="flex flex-col border-r border-dashed">
-        <div className="text-xl p-2 border-b border-dashed bg-card">
+    <div className="max-w-4xl w-full mx-auto text-sm border-b border-x border-dashed grid grid-cols-5">
+      <div className="flex flex-col border-r border-dashed col-span-2">
+        <NextLink
+          href="/"
+          className="text-xl p-4 border-b border-dashed active:font-normal font-extrabold font-mono duration-200 ease-out hover:bg-card active:brightness-75"
+        >
           Ingo Wolf
-        </div>
+        </NextLink>
         <div className="flex flex-row">
           <NextLink
             href="mailto:me@ingo.au"
             target="_blank"
-            className="border-r border-dashed p-2 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            className="border-r border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
           >
             <Mail className="size-4" />
           </NextLink>
           <NextLink
             href="https://github.com/Inglan"
             target="_blank"
-            className="border-r border-dashed p-2 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            className="border-r border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
           >
             <FaGithub className="size-4" />
           </NextLink>
           <NextLink
             href="https://signal.me/#eu/aON_wvkns7bfeU-UAj_09B1Yym8WVC2QIWWV8rIhYZzPc2xGLUtBeLWMc9LJoWNB"
             target="_blank"
-            className="border-r border-dashed p-2 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            className="border-r border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
           >
             <SiSignal className="size-4" />
           </NextLink>
           <NextLink
             href="/contact"
-            className="border-r border-dashed p-2 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            className="border-r border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
           >
             <ArrowRight className="size-4" />
           </NextLink>
+          <div className="grow bg-striped-gradient bg-size-[80px_80px]"></div>
         </div>
       </div>
       {/*<div className="flex flex-row w-fit items-center  mx-auto">
