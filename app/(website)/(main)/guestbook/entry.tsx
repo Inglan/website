102c7ee1bdc2af6076fc1039dc2df4ceed5043e6
Ingo Wolf <me@ingowolf.au>
2025-12-18 14:14:57 +1100

    add time

--- app/(website)/(main)/guestbook/entry.tsx
+++ app/(website)/(main)/guestbook/entry.tsx
@@ -1,3 +1,4 @@
+import { FormattedDateTime } from "@/components/formatted-date";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { FunctionReturnType } from "convex/server";
@@ -10,6 +11,12 @@ export default function Entry({
   return (
     <div className="p-4 border-b border-dashed" key={entry.id}>
       <div className="text-primary text-xl">{entry.name}</div>
+      <div className="text-sm">
+        <FormattedDateTime
+          date={new Date(entry.creationTime).toISOString()}
+          format="DATETIME"
+        />
+      </div>
       <div className="text-lg">{entry.message}</div>
     </div>
   );
