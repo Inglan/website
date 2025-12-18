b59dfc3178257799b5ca273b77fbf2f0193d5d5a
Ingo Wolf <me@ingowolf.au>
2025-12-18 11:26:58 +1100

    add guestbook page

---
+++ app/(website)/(main)/guestbook/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Header from "@/components/header";
+
+export default function Page() {
+  return (
+    <div className="max-w-4xl w-full mx-auto border border-t-0 border-dashed">
+      <Header>Guestbook</Header>
+    </div>
+  );
+}
