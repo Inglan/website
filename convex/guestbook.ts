edf9ea33002709e597169dbae73b2d0741709ece
Ingo Wolf <me@ingowolf.au>
2025-12-18 14:22:50 +1100

    send back pending stuff

--- convex/guestbook.ts
+++ convex/guestbook.ts
@@ -21,10 +21,22 @@ export const add = mutation({
 
 export const get = query({
   handler: async (ctx) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
     const entries = await ctx.db
       .query("guestbookEntries")
       .withIndex("by_status", (q) => q.eq("status", "approved"))
       .collect();
+    if (user) {
+      entries.push(
+        ...(await ctx.db
+          .query("guestbookEntries")
+          .withIndex("by_status_user", (q) =>
+            q.eq("status", "pending").eq("userId", user._id),
+          )
+          .collect()),
+      );
+    }
+
     return await Promise.all(
       entries.map(async (entry) => ({
         id: entry._id,
