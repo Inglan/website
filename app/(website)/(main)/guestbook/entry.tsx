708930343a6b6e6d07cc017a88ce0d9190cb2eb6
Ingo Wolf <me@ingowolf.au>
2025-12-18 14:26:21 +1100

    change style based on state

--- app/(website)/(main)/guestbook/entry.tsx
+++ app/(website)/(main)/guestbook/entry.tsx
@@ -1,6 +1,6 @@
 import { FormattedDateTime } from "@/components/formatted-date";
 import { api } from "@/convex/_generated/api";
-import { Doc } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
 import { FunctionReturnType } from "convex/server";
 
 export default function Entry({
@@ -9,7 +9,18 @@ export default function Entry({
   entry: FunctionReturnType<typeof api.guestbook.get>[number];
 }) {
   return (
-    <div className="p-4 border-b border-dashed" key={entry.id}>
+    <div
+      className={cn(
+        "p-4 border-b border-dashed",
+        entry.status == "pending"
+          ? "bg-striped-gradient bg-size-[80px_80px] opacity-75"
+          : "",
+      )}
+      key={entry.id}
+    >
+      {entry.status == "pending" && (
+        <div className="text-sm text-neutral-400">Pending Approval</div>
+      )}
       <div className="text-primary text-xl">{entry.name}</div>
       <div className="text-sm">
         <FormattedDateTime
