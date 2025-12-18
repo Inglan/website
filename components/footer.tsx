3b152defadeabcfed6fb396384f8ef0e0ae5bafb
Ingo Wolf <me@ingowolf.au>
2025-12-18 22:41:57 +1100

    add better footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,90 +8,100 @@ import { MENU_ITEMS, OTHER_PAGES } from "@/lib/constants";
 
 export function Footer() {
   return (
-    <div className="max-w-4xl w-full mx-auto text-sm sm:border-b border-l border-dashed flex flex-col sm:flex-row">
-      <div className="flex flex-col border-r border-dashed col-span-3">
-        <NextLink
-          href="/"
-          className="flex text-xl p-4 border-b border-dashed active:font-normal font-extrabold font-mono duration-200 ease-out hover:bg-card active:brightness-75"
-        >
-          Ingo Wolf
-        </NextLink>
-        <div className="flex flex-row">
+    <>
+      <div className="max-w-4xl w-full mx-auto text-sm sm:border-b border-l border-dashed flex flex-col sm:flex-row">
+        <div className="flex flex-col border-r border-dashed col-span-3">
           <NextLink
-            href="mailto:me@ingo.au"
-            target="_blank"
-            className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            href="/"
+            className="flex text-xl p-4 border-b border-dashed active:font-normal font-extrabold font-mono duration-200 ease-out hover:bg-card active:brightness-75"
           >
-            <span className="sr-only">Email</span>
-            <Mail className="size-4" />
+            Ingo Wolf
           </NextLink>
-          <NextLink
-            href="https://github.com/Inglan"
-            target="_blank"
-            className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
-          >
-            <span className="sr-only">GitHub</span>
-            <FaGithub className="size-4" />
-          </NextLink>
-          <NextLink
-            href="https://signal.me/#eu/aON_wvkns7bfeU-UAj_09B1Yym8WVC2QIWWV8rIhYZzPc2xGLUtBeLWMc9LJoWNB"
-            target="_blank"
-            className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
-          >
-            <span className="sr-only">Signal</span>
-            <SiSignal className="size-4" />
-          </NextLink>
-          <NextLink
-            href="/contact"
-            className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
-          >
-            <span className="sr-only">Contact</span>
-            <ArrowRight className="size-4" />
-          </NextLink>
-          <div className="border-b border-dashed grow bg-striped-gradient bg-size-[80px_80px] bg-fixed min-w-20"></div>
-        </div>
-        <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10"></div>
-      </div>
-      <div className="hidden sm:block bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed grow"></div>
-      <div className="flex flex-col min-w-64">
-        <div className="flex-col grid grid-cols-2 h-fit">
-          {[...MENU_ITEMS, ...OTHER_PAGES].map((item) => (
+          <div className="flex flex-row">
             <NextLink
-              key={item.url}
-              href={item.url}
-              className="w-full p-2 border-b border-r border-dashed duration-200 ease-out hover:bg-card active:brightness-75"
+              href="mailto:me@ingo.au"
+              target="_blank"
+              className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
             >
-              {item.label}
+              <span className="sr-only">Email</span>
+              <Mail className="size-4" />
             </NextLink>
-          ))}
+            <NextLink
+              href="https://github.com/Inglan"
+              target="_blank"
+              className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            >
+              <span className="sr-only">GitHub</span>
+              <FaGithub className="size-4" />
+            </NextLink>
+            <NextLink
+              href="https://signal.me/#eu/aON_wvkns7bfeU-UAj_09B1Yym8WVC2QIWWV8rIhYZzPc2xGLUtBeLWMc9LJoWNB"
+              target="_blank"
+              className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            >
+              <span className="sr-only">Signal</span>
+              <SiSignal className="size-4" />
+            </NextLink>
+            <NextLink
+              href="/contact"
+              className="border-r border-b  border-dashed p-4 aspect-square w-fit flex justify-center items-center duration-200 ease-out hover:bg-card active:brightness-75"
+            >
+              <span className="sr-only">Contact</span>
+              <ArrowRight className="size-4" />
+            </NextLink>
+            <div className="border-b border-dashed grow bg-striped-gradient bg-size-[80px_80px] bg-fixed min-w-20"></div>
+          </div>
+          <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10"></div>
         </div>
-        <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10 border-r border-dashed"></div>
+        <div className="hidden sm:block bg-striped-gradient bg-size-[80px_80px] bg-fixed border-r border-dashed grow"></div>
+        <div className="flex flex-col min-w-64">
+          <div className="flex-col grid grid-cols-2 h-fit">
+            {[...MENU_ITEMS, ...OTHER_PAGES].map((item) => (
+              <NextLink
+                key={item.url}
+                href={item.url}
+                className="w-full p-2 border-b border-r border-dashed duration-200 ease-out hover:bg-card active:brightness-75"
+              >
+                {item.label}
+              </NextLink>
+            ))}
+          </div>
+          <div className="hidden sm:block h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed min-h-10 border-r border-dashed"></div>
+        </div>
       </div>
-      {/*<div className="flex flex-row w-fit items-center  mx-auto">
-        <Button variant="ghost" size="icon" asChild>
+      <div className="max-w-4xl w-full mx-auto flex flex-col-reverse sm:flex-row border-b border-x border-dashed items-center">
+        <div className="px-8 h-14 sm:border-r border-dashed flex items-center justify-center w-full sm:w-fit border-t sm:border-t-0">
+          <span>
+            This website is{" "}
+            <Link href="https://github.com/Inglan/website" target="_blank">
+              open source
+            </Link>
+          </span>
+        </div>
+        <div className="hidden sm:block grow h-14 bg-striped-gradient bg-size-[80px_80px] bg-fixed"></div>
+        <div className="flex flex-row w-full sm:w-fit">
           <Link
             href="https://ctp-webr.ing/ingo/previous"
-            className="aspect-square h-full"
+            className="aspect-square h-14 flex items-center justify-center duration-200 ease-out hover:bg-card active:brightness-75 sm:border-l border-dashed"
           >
             <span className="sr-only">Previous</span>
-            <ArrowLeft />
+            <ArrowLeft className="size-4" />
           </Link>
-        </Button>
-        <Button variant="outline" className="border-dashed" asChild>
-          <Link href="https://ctp-webr.ing/">Catppuccin webring</Link>
-        </Button>
-        <Button variant="ghost" size="icon" asChild>
-          <Link href="https://ctp-webr.ing/ingo/next">
+          <Link
+            href="https://ctp-webr.ing/ingo/previous"
+            className="h-14 px-4 flex items-center justify-center duration-200 ease-out hover:bg-card active:brightness-75 border-l border-dashed grow"
+          >
+            Catppuccin Webring
+          </Link>
+          <Link
+            href="https://ctp-webr.ing/ingo/next"
+            className="aspect-square h-14 flex items-center justify-center duration-200 ease-out hover:bg-card active:brightness-75 border-l border-dashed"
+          >
             <span className="sr-only">Next</span>
-            <ArrowRight />
+            <ArrowRight className="size-4" />
           </Link>
-        </Button>
+        </div>
       </div>
-      <br />
-      This website is{" "}
-      <Link href="https://github.com/Inglan/website" target="_blank">
-        open source
-      </Link>*/}
-    </div>
+    </>
   );
 }
