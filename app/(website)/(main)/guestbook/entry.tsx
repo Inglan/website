7c78040285482c2371f73813a0e1a59de13ddfea
Ingo Wolf <me@ingowolf.au>
2025-12-18 16:36:40 +1100

    update text

--- app/(website)/(main)/guestbook/entry.tsx
+++ app/(website)/(main)/guestbook/entry.tsx
@@ -96,7 +96,7 @@ export default function Entry({
                         );
                       }}
                     >
-                      Delete
+                      Ban
                     </Button>
                   </DialogClose>
                 </DialogFooter>
