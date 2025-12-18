6806a03f2127f670b4b0c36689e33a8d554b75dd
Ingo Wolf <me@ingowolf.au>
2025-12-18 11:27:23 +1100

    disable creds auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -18,8 +18,7 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
     emailAndPassword: {
-      enabled: true,
-      requireEmailVerification: false,
+      enabled: false,
     },
     plugins: [
       // The Convex plugin is required for Convex compatibility
