5cb0017da3a0e6026e5e7cd38dd46fdb720efbe4
Ingo Wolf <me@ingowolf.au>
2025-12-18 14:29:39 +1100

    send back user id

--- convex/guestbook.ts
+++ convex/guestbook.ts
@@ -45,6 +45,7 @@ export const get = query({
           name: (await authComponent.getAnyUserById(ctx, entry.userId))?.name,
           creationTime: entry._creationTime,
           status: entry.status,
+          userId: entry.userId,
         })),
       )
     ).reverse();
