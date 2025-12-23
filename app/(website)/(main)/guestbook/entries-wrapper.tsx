11ce3d66add1240eadfc8c21fedf17ef5b7a7582
Ingo Wolf <me@ingowolf.au>
2025-12-23 18:30:24 +1100

    fix

--- app/(website)/(main)/guestbook/entries-wrapper.tsx
+++ app/(website)/(main)/guestbook/entries-wrapper.tsx
@@ -2,8 +2,13 @@
 
 import { preloadQuery } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
+import { Entries } from "./entries";
 
 export async function EntriesWrapper() {
   const preloadedEntries = await preloadQuery(api.guestbook.get, {});
-  return <></>;
+  return (
+    <>
+      <Entries preloadedEntries={preloadedEntries} />
+    </>
+  );
 }
