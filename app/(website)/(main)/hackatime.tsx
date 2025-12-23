03413425c1e95a8d4232e489c65fe12a5b8755b4
Ingo Wolf <me@ingowolf.au>
2025-12-23 20:43:17 +1100

    add example stats

--- app/(website)/(main)/hackatime.tsx
+++ app/(website)/(main)/hackatime.tsx
@@ -1,7 +1,37 @@
+import { cn } from "@/lib/utils";
+
 export default function Hackatime() {
+  const quickStats = [
+    {
+      title: "Total Coding Time",
+      value: "272h 38m",
+      className: "col-span-2 bg-striped-gradient bg-size-[80px_80px]",
+    },
+    {
+      title: "This Week",
+      value: "10h 45m",
+    },
+    {
+      title: "Today",
+      value: "1h 30m",
+    },
+  ];
   return (
-    <div>
-      <h1>Hackatime</h1>
+    <div className="w-full mx-auto border-b border-dashed max-w-4xl">
+      <div className="grid grid-cols-4 border-l border-dashed">
+        {quickStats.map((stat, index) => (
+          <div
+            key={index}
+            className={cn(
+              stat.className,
+              "p-4 flex flex-col border-r border-dashed",
+            )}
+          >
+            <div className="text-4xl text-primary">{stat.value}</div>
+            <div>{stat.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
