1c6bf5344861e9c3b1fccb394ed7328f3b9d2ba7
Ingo Wolf <me@ingowolf.au>
2025-12-23 23:49:10 +1100

    add http route

--- convex/http.ts
+++ convex/http.ts
@@ -1,8 +1,15 @@
 import { httpRouter } from "convex/server";
 import { authComponent, createAuth } from "./auth";
+import { createIncidentFromIPS } from "./networkingIncidents";
 
 const http = httpRouter();
 
 authComponent.registerRoutes(http, createAuth);
+
+http.route({
+  path: "/networking/incidents/ips",
+  method: "POST",
+  handler: createIncidentFromIPS,
+});
 
 export default http;
