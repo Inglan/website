040385b7cea11af6839305fc126bf481ca53c32c
Ingo Wolf <me@ingowolf.au>
2025-12-31 15:54:37 +1100

    add copy button

--- app/(website)/(main)/ips/list.tsx
+++ app/(website)/(main)/ips/list.tsx
@@ -11,6 +11,8 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { FormattedDateTime } from "@/components/formatted-date";
 import { cn } from "@/lib/utils";
+import { Clipboard } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function IPsList(props: {
   preloadedIPs: Preloaded<typeof api.networkingIncidents.get>;
@@ -21,21 +23,29 @@ export default function IPsList(props: {
     <Accordion type="multiple" className="border-b border-dashed">
       {ips.map((ip) => (
         <AccordionItem key={ip.ip} value={ip.ip} className="border-dashed">
-          <AccordionTrigger className="px-4 bg-card no-underline! cursor-pointer duration-200 ease-out hover:bg-card active:brightness-75 h-14 py-0 items-center">
-            <div className="flex flex-row gap-2 items-center">
-              {ip.ip}{" "}
-              <Badge variant="outline">
-                {ip.incidents.length} incident
-                {ip.incidents.length === 1 ? "" : "s"}
-              </Badge>
-              <Badge variant="outline">
-                <FormattedDateTime
-                  format="DATETIME"
-                  date={new Date(ip.incidents[0]._creationTime).toISOString()}
-                />
-              </Badge>
-            </div>
-          </AccordionTrigger>
+          <div className="flex flex-row [&>h3]:grow">
+            <AccordionTrigger className="px-4 bg-card no-underline! cursor-pointer duration-200 ease-out hover:bg-card active:brightness-75 h-12 py-0 items-center">
+              <div className="flex flex-row gap-2 items-center">
+                {ip.ip}{" "}
+                <Badge variant="outline">
+                  {ip.incidents.length} incident
+                  {ip.incidents.length === 1 ? "" : "s"}
+                </Badge>
+                <Badge variant="outline">
+                  <FormattedDateTime
+                    format="DATETIME"
+                    date={new Date(ip.incidents[0]._creationTime).toISOString()}
+                  />
+                </Badge>
+              </div>
+            </AccordionTrigger>
+            <Button
+              className="border-l border-dashed h-12 w-12 cursor-pointer duration-200 ease-out hover:bg-card active:brightness-75"
+              variant="ghost"
+            >
+              <Clipboard />
+            </Button>
+          </div>
           <AccordionContent className="p-0">
             {ip.incidents.map((incident, index) => (
               <div
