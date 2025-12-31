defffa3a36bcbc72764c095914fc8abe500d4606
Ingo Wolf <me@ingowolf.au>
2025-12-31 14:18:36 +1100

    update mutation to add to db better

--- convex/networkingIncidents.ts
+++ convex/networkingIncidents.ts
@@ -2,13 +2,18 @@ import { v } from "convex/values";
 import { httpAction, internalMutation } from "./_generated/server";
 import { internal } from "./_generated/api";
 import { networkIncidents } from "./schema";
+import { UniFiWebhookBody } from "./types";
 
 export const createIncidentFromIPS = httpAction(async (ctx, request) => {
-  const body = await request.json();
-  await ctx.runMutation(internal.networkingIncidents.createIncident, {
-    source: "intrusionPrevention",
-    rawData: JSON.stringify(body),
-  });
+  const body = (await request.json()) as UniFiWebhookBody;
+  if (body.severity > 7) {
+    await ctx.runMutation(internal.networkingIncidents.createIncident, {
+      source: "intrusionPrevention",
+      ip: body.parameters.src,
+      details: `${body.message}\nPolicy Name: ${body.parameters.UNIFIpolicyName}\nSignature: ${body.parameters.UNIFIipsSignature}`,
+      rawData: JSON.stringify(body),
+    });
+  }
   return new Response("yes", { status: 200 });
 });
 export const createIncident = internalMutation({
