218b28f41be1eaeff30325df31bf216210251049
Ingo Wolf <me@ingowolf.au>
2025-12-31 15:10:29 +1100

    add ips page

---
+++ app/(website)/(main)/ips/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header";
+
+export default function IPs() {
+  return (
+    <div className="max-w-4xl w-full mx-auto border-x border-dashed">
+      <Header>IP Addresses</Header>
+    </div>
+  );
+}
