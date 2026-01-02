68a38e21158a1ad846d2f54646f06ad8b940b373
Ingo Wolf <me@ingowolf.au>
2026-01-02 16:08:34 +1100

    create component for navbar container

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,42 +10,54 @@ import { useScroll, useSpring } from "motion/react";
 import { motion } from "motion/react";
 import Image from "next/image";
 
-export default function Navbar() {
-  const setSearchOpen = useUiState((state) => state.setSearchOpen);
-
+function NavbarContainer({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="w-full sticky top-0 z-50">
       <div className="w-full max-w-4xl mx-auto pt-4 border-x border-dashed bg-striped-gradient bg-size-[80px_80px] bg-fixed bg-background">
         <nav className="w-full max-w-4xl mx-auto border-y border-dashed flex flex-row bg-background">
-          <MenuLink
-            href="/"
-            containerClassName="border-l-0 border-r"
-            className="text-xl py-4! px-4! active:font-normal font-extrabold min-w-36 font-mono"
-            animation={false}
-          >
-            Ingo Wolf
-          </MenuLink>
-          <div className="grow bg-striped-gradient bg-size-[80px_80px] bg-fixed"></div>
-          <div className="hidden md:flex flex-row">
-            {MENU_ITEMS.map((item) => (
-              <MenuLink key={item.url} href={item.url}>
-                {item.label}
-              </MenuLink>
-            ))}
-          </div>
-          <MenuLink
-            className="px-6!"
-            onClick={() => {
-              setSearchOpen(true);
-            }}
-          >
-            <span className="sr-only">Menu</span>
-            <Menu className="size-4 md:hidden" />
-            <Search className="size-4 md:block hidden" />
-          </MenuLink>
+          {children}
         </nav>
       </div>
     </div>
+  );
+}
+
+export default function Navbar() {
+  const setSearchOpen = useUiState((state) => state.setSearchOpen);
+
+  return (
+    <NavbarContainer>
+      <MenuLink
+        href="/"
+        containerClassName="border-l-0 border-r"
+        className="text-xl py-4! px-4! active:font-normal font-extrabold min-w-36 font-mono"
+        animation={false}
+      >
+        Ingo Wolf
+      </MenuLink>
+      <div className="grow bg-striped-gradient bg-size-[80px_80px] bg-fixed"></div>
+      <div className="hidden md:flex flex-row">
+        {MENU_ITEMS.map((item) => (
+          <MenuLink key={item.url} href={item.url}>
+            {item.label}
+          </MenuLink>
+        ))}
+      </div>
+      <MenuLink
+        className="px-6!"
+        onClick={() => {
+          setSearchOpen(true);
+        }}
+      >
+        <span className="sr-only">Menu</span>
+        <Menu className="size-4 md:hidden" />
+        <Search className="size-4 md:block hidden" />
+      </MenuLink>
+    </NavbarContainer>
   );
 }
 
@@ -60,38 +72,34 @@ export function PostNavbar({ title }: { title: string }) {
   });
 
   return (
-    <div className="w-full sticky top-0 z-50">
-      <div className="w-full max-w-4xl mx-auto pt-4 border-x border-dashed bg-striped-gradient bg-size-[80px_80px] bg-fixed bg-background">
-        <nav className="w-full max-w-4xl mx-auto border-y border-dashed flex flex-row relative bg-background">
-          <MenuLink
-            href="./"
-            className="px-6!"
-            containerClassName="border-l-0"
-            animation={false}
-          >
-            <span className="sr-only">Back</span>
-            <ArrowLeft className="size-4" />
-          </MenuLink>
-          <div className="grow flex items-center min-h-full py-4 px-2 text-xl">
-            <p className="line-clamp-1">{title}</p>
-          </div>
-          <MenuLink
-            className="px-6!"
-            containerClassName="border-l-0"
-            onClick={() => {
-              setSearchOpen(true);
-            }}
-          >
-            <span className="sr-only">Menu</span>
-            <Menu className="size-4" />
-          </MenuLink>
-          <motion.div
-            className="top-0 left-0 absolute w-full h-full bg-popover -z-10 origin-left "
-            style={{ scaleX }}
-          ></motion.div>
-        </nav>
+    <NavbarContainer>
+      <MenuLink
+        href="./"
+        className="px-6!"
+        containerClassName="border-l-0"
+        animation={false}
+      >
+        <span className="sr-only">Back</span>
+        <ArrowLeft className="size-4" />
+      </MenuLink>
+      <div className="grow flex items-center min-h-full py-4 px-2 text-xl">
+        <p className="line-clamp-1">{title}</p>
       </div>
-    </div>
+      <MenuLink
+        className="px-6!"
+        containerClassName="border-l-0"
+        onClick={() => {
+          setSearchOpen(true);
+        }}
+      >
+        <span className="sr-only">Menu</span>
+        <Menu className="size-4" />
+      </MenuLink>
+      <motion.div
+        className="top-0 left-0 absolute w-full h-full bg-popover -z-10 origin-left "
+        style={{ scaleX }}
+      ></motion.div>
+    </NavbarContainer>
   );
 }
 
