fa901843a8e1f20e297cdef13dc48c00fb054c05
Ingo Wolf <me@ingowolf.au>
2026-01-02 16:03:48 +1100

    wrap everything

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -10,11 +10,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      {children}
+    <div className="px-4">
+      <div className="max-w-4xl mx-auto w-full bg-background">{children}</div>
       <StripedSeparator />
       <Footer />
       <div className="w-full h-full bg-striped-gradient bg-size-[80px_80px] bg-fixed fixed top-0 left-0 -z-50 opacity-25"></div>
-    </>
+    </div>
   );
 }
