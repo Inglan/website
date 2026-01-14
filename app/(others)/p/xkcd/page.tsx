0429b241c20cce8891b67213d366bedc7bde091c
Ingo Wolf <me@ingowolf.au>
2026-01-14 14:47:24 +1100

    add links

--- app/(others)/p/xkcd/page.tsx
+++ app/(others)/p/xkcd/page.tsx
@@ -1,11 +1,24 @@
 import ProjectPage from "@/components/project-other-page";
+import { ExternalLink } from "lucide-react";
+import { FaGithub } from "react-icons/fa";
 
 export default function XKCDPage() {
   return (
     <ProjectPage
       name="xkcd-webapp"
       description="A web application that displays xkcd comics"
-      links={[]}
+      links={[
+        {
+          title: "GitHub",
+          icon: <FaGithub />,
+          url: "https://github.com/ingoau/xkcd-webapp",
+        },
+        {
+          title: "Demo",
+          icon: <ExternalLink />,
+          url: "https://xkcd.ingo.au/",
+        },
+      ]}
     />
   );
 }
