91c9cb7400edfc4b3fa2bf0b52e9094f15bcc152
Ingo Wolf <me@ingowolf.au>
2026-01-14 14:41:41 +1100

    add more sections

--- app/(others)/p/xkcd/page.tsx
+++ app/(others)/p/xkcd/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function XKCDPage() {
   return (
-    <div className="flex-col p-4 absolute w-full h-screen flex items-center">
-      <div className="border border-dashed p-4 w-full max-w-2xl"></div>
+    <div className="flex-col p-8 absolute w-full h-screen flex items-center">
+      <div className="border border-dashed w-full max-w-2xl">
+        <div className="p-4 border-b border-dashed flex flex-col gap-2">
+          <p>
+            <Link href="/">
+              <span className="text-primary">ingo.au</span>/
+            </Link>
+          </p>
+          <h1 className="text-4xl lg:text-5xl text-primary">xkcd-webapp</h1>
+        </div>
+        <p className="border-b border-dashed">e</p>
+      </div>
     </div>
   );
 }
