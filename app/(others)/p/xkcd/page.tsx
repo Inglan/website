28406db36c44699f49693dd2ea527d9d14cab163
Ingo Wolf <me@ingowolf.au>
2026-01-14 14:37:20 +1100

    actually scrap that

--- app/(others)/p/xkcd/page.tsx
+++ app/(others)/p/xkcd/page.tsx
@@ -1,10 +1,7 @@
 export default function XKCDPage() {
   return (
-    <div className="flex flex-col p-4 absolute w-full h-screen">
-      <div className="border-r border-dashed p-4 absolute top-0 left-0 h-screen"></div>
-      <div className="border-l border-dashed p-4 absolute top-0 right-0 h-screen"></div>
-      <div className="border-b border-dashed p-4 absolute top-0 left-0 w-screen"></div>
-      <div className="border-t border-dashed p-4 absolute bottom-0 left-0 w-screen"></div>
+    <div className="flex-col p-4 absolute w-full h-screen flex items-center">
+      <div className="border border-dashed p-4"></div>
     </div>
   );
 }
