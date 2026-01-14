c4280d7095d2cb58ac64b773c7ef5119b79ca9e5
Ingo Wolf <me@ingowolf.au>
2026-01-14 14:37:37 +1100

    add width for div

--- app/(others)/p/xkcd/page.tsx
+++ app/(others)/p/xkcd/page.tsx
@@ -1,7 +1,7 @@
 export default function XKCDPage() {
   return (
     <div className="flex-col p-4 absolute w-full h-screen flex items-center">
-      <div className="border border-dashed p-4"></div>
+      <div className="border border-dashed p-4 w-full max-w-2xl"></div>
     </div>
   );
 }
