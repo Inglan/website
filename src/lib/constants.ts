146b2e11fec60743ad8340af39372b1d8dc8a1e3
Ingo Wolf <me@ingowolf.au>
2026-02-02 11:57:34 +1100

    rename constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@
 
 // export const NICE_EASE: Easing | Easing[] = [0.165, 0.84, 0.44, 1.0];
 
-export const MENU_ITEMS = [
+export const NAVBAR_ITEMS = [
   { url: "/", label: "Home" },
   { url: "/projects", label: "Projects" },
   { url: "/blog", label: "Blog" },
