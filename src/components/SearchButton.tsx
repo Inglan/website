2b36cde2fea08380d79714c36f6ee25bbab4b583
Ingo Wolf <me@ingowolf.au>
2026-02-06 18:17:29 +1100

    add updating state in search button

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,9 +1,15 @@
 import { Search } from "lucide-react";
+import { isMenuOpen } from "@/lib/state";
 
 export default function SearchButton() {
   return (
     <div className="border-l w-fit">
-      <button className="menu-item btn-interactive px-6 cursor-pointer">
+      <button
+        className="menu-item btn-interactive px-6 cursor-pointer"
+        onClick={() => {
+          isMenuOpen.set(true);
+        }}
+      >
         <Search className="size-4" />
       </button>
     </div>
