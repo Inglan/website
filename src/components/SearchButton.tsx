10ef1620c5b4f0d64896b6f2451bf765ffe38245
Ingo Wolf <me@ingowolf.au>
2026-02-06 18:13:31 +1100

    add cursorpointer

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -3,7 +3,7 @@ import { Search } from "lucide-react";
 export default function SearchButton() {
   return (
     <div className="border-l w-fit">
-      <button className="menu-item btn-interactive px-6">
+      <button className="menu-item btn-interactive px-6 cursor-pointer">
         <Search className="size-4" />
       </button>
     </div>
