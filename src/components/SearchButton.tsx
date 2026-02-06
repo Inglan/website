617c8b14c72f9943550114e76f806b2ee4151f29
Ingo Wolf <me@ingowolf.au>
2026-02-06 18:13:16 +1100

    update search button a bti

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,3 +1,11 @@
+import { Search } from "lucide-react";
+
 export default function SearchButton({ className = "" }) {
-  return <button className={className}>Search</button>;
+  return (
+    <div className="border-l w-fit">
+      <button className="menu-item btn-interactive px-6">
+        <Search className="size-4" />
+      </button>
+    </div>
+  );
 }
